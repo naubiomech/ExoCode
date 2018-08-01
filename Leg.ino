67a350fcf7c3d36b9e37d18ba9bb3e99732f28c8
Jack Austin Garrard <jg2562@nau.edu>
2018-07-31 17:42:16 -0700

    Moved leg code out of header

---
+++ Leg.ino
@@ -0,0 +1,37 @@
+
+void initialize_leg(Leg* leg) {
+  pinMode(leg->pin_err, INPUT);
+  pinMode(leg->torque_sensor_ankle_pin, INPUT); //enable the torque reading of the left torque sensor
+ 
+  analogWrite(leg->motor_ankle_pin, zero);
+  leg->pid.SetMode(AUTOMATIC);
+  leg->pid.SetTunings(leg->kp, leg->ki, leg->kd);                                      //Kp, Ki, Kd ##COULD BE AUTOTUNED
+  leg->pid.SetOutputLimits(-250, 250);                                  //range of Output around 0 ~ 1995 ##THIS IS DIFFERENT NOW AND SHOULD CONCRETELY CONFIRM
+  leg->pid.SetSampleTime(PID_sample_time);  
+
+  leg->p_steps->fsr_Toe = leg->fsr_sense_Toe;
+}
+
+void initialize_left_leg(Leg* left_leg) {
+  left_leg->pin_err = MOTOR_ERROR_LEFT_ANKLE_PIN;
+  left_leg->fsr_sense_Heel = FSR_SENSE_LEFT_HEEL_PIN;
+  left_leg->fsr_sense_Toe = FSR_SENSE_LEFT_TOE_PIN;
+  left_leg->address_torque = 0;
+  left_leg->address_FSR = 18;
+  left_leg->p_steps = &val_L;
+  left_leg->torque_sensor_ankle_pin = TORQUE_SENSOR_LEFT_ANKLE_PIN;
+  left_leg->motor_ankle_pin = MOTOR_LEFT_ANKLE_PIN;
+  initialize_leg(left_leg);
+}
+
+void initialize_right_leg(Leg* right_leg) {
+  right_leg->pin_err = MOTOR_ERROR_RIGHT_ANKLE_PIN;
+  right_leg->fsr_sense_Heel = FSR_SENSE_RIGHT_HEEL_PIN;
+  right_leg->fsr_sense_Toe = FSR_SENSE_RIGHT_TOE_PIN;
+  right_leg->address_torque = 9;
+  right_leg->address_FSR = 36;
+  right_leg->p_steps = &val_R;
+  right_leg->torque_sensor_ankle_pin = TORQUE_SENSOR_RIGHT_ANKLE_PIN;
+  right_leg->motor_ankle_pin = MOTOR_RIGHT_ANKLE_PIN;
+  initialize_leg(right_leg);
+}
