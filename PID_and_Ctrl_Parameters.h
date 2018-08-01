5ace1edd6ad0eff31fb8c214e9c9b6c07de56020
Jack Austin Garrard <jg2562@nau.edu>
2018-07-31 17:40:22 -0700

    Added header guard to PID_and_Ctrl_Parameters.h

--- PID_and_Ctrl_Parameters.h
+++ PID_and_Ctrl_Parameters.h
@@ -1,5 +1,8 @@
 //All the needed parameters to set torque bias, PID ctrl, to enable the motors and to average the torque signals
+#ifndef PID_AND_CTRL_PARAMETERS_HEADER
+#define PID_AND_CTRL_PARAMETERS_HEADER
 int count = 0;
 
 //Includes the PID library so we can utilize PID control
 int PID_sample_time = 1;                                             //PID operates at 1000Hz, calling at a freq of 1 ms.
+#endif
