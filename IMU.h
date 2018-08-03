58c2b305ab91cc74ce8bc01c3a79405372f5ef45
Jack Austin Garrard <jg2562@nau.edu>
2018-08-03 11:25:28 -0700

    Moved the IMU parameters as well

--- IMU.h
+++ IMU.h
@@ -6,12 +6,10 @@
 #include <Adafruit_BNO055_t3.h>
 #include <utility/imumaths.h>
 
-const int BNO055_SAMPLERATE_DELAY_MS = 100;
 sensors_event_t event;
 imu::Vector<3> euler;
 
-double stability_trq;
-const double stability_trq_gain = 0.01;
+volatile double stability_trq;
 
 Adafruit_BNO055 bno = Adafruit_BNO055(WIRE_BUS, 1, BNO055_ADDRESS_A, I2C_MASTER, IMU_1_PINS, I2C_PULLUP_EXT, I2C_RATE_100, I2C_OP_MODE_ISR);
 
