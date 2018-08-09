1564d62c1b0657afb3ddef549053a03078f5448b
Jack Garrard <jg2562@nau.edu>
2018-08-09 14:59:54 -0700

    Added header guard to IMU Header

--- IMU.h
+++ IMU.h
@@ -1,3 +1,5 @@
+#ifndef IMU_HEADER
+#define IMU_HEADER
 
 #include <i2c_t3.h>
 #include <Adafruit_Sensor.h>
@@ -10,3 +12,4 @@ imu::Vector<3> eulerTwo;
 Adafruit_BNO055 bnoTwo = Adafruit_BNO055(WIRE1_BUS, 1, 0x28, I2C_MASTER, I2C_PINS_3_4, I2C_PULLUP_EXT, I2C_RATE_100, I2C_OP_MODE_ISR);
 
 bool IMU_flag;
+#endif
