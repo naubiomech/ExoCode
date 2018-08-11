2d7bc8739472f028420b49bd83afaa6da80c262f
Jack Garrard <jg2562@nau.edu>
2018-08-10 18:16:35 -0700

    Added Giamma's changes

--- pid.ino
+++ pid.ino
@@ -2,19 +2,30 @@
 #include "Leg.h"
 #include "Board.h"
 
-void pid(Leg* leg, double trq, double stability){
+void pid(Leg* leg, double meas_trq, double meas_IMU) {
 
   PID* pid;
-  double input;
+  double PID_ref;
 
-  if (IMU_ENABLED && leg->state == 3 && Trq_time_volt == 2){
-    input = stability * leg->Prop_Gain;
+  if (IMU_ENABLED && leg->state == 3 && Trq_time_volt == 2) {
+    if (meas_IMU >= 45)
+    {
+      meas_IMU = 45;
+    }
+    else if (meas_IMU <= -45) {
+      meas_IMU = -45;
+    }
+    PID_ref = 0;
+    leg->PID_Setpoint = PID_ref;
+    leg->Input = meas_IMU * leg->Prop_Gain; // this is totally new, we have to test!
+    //    PID_ref = meas_IMU * leg->Prop_Gain;
     pid = &(leg->balance_pid);
-  } else{
-    input = trq;
+  } else {
+    PID_ref = leg->PID_Setpoint;
+    leg->Input = meas_trq;
     pid = &(leg->ankle_pid);
 
-    if ((abs(input) > 25))
+    if ((abs(meas_trq) > 25))
     {
       leg->KF = 0;
       double old_L_state_L = leg->state;
@@ -29,8 +40,17 @@ void pid(Leg* leg, double trq, double stability){
     }
   }
 
-  leg->Input = input;
+  //  leg->Input = PID_ref;
   pid->Compute_KF(leg->KF);
+
+  //This can be used as alternative to the previous gain (see up)
+  if (IMU_ENABLED && leg->state == 3 && Trq_time_volt == 2) {
+    leg->Output *= leg->Prop_Gain;
+    if (leg->Output >= 1500) leg->Output = 1500;
+    if (leg->Output <= -1500) leg->Output = -1500;
+    //    leg->PID_Setpoint = 56.5 / (2.1) * ((leg->Output) * (3.3 / 4096));
+  }
+
   leg->Vol = leg->Output + zero; //need to map
   analogWrite(leg->motor_ankle_pin, leg->Vol); //0 to 4096 writing for motor to get Input
 }
