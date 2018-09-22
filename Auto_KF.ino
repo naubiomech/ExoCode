f0c3b869cc91f51afe3e2bbdef2c0d3e244f4441
Jack Garrard <jg2562@nau.edu>
2018-09-22 16:11:38 -0700

    Broke auto kf into several functions

--- Auto_KF.ino
+++ Auto_KF.ino
@@ -1,50 +1,49 @@
 #include "Auto_KF.h"
 #include "Leg.h"
 
-void sub_Auto_KF(Leg* leg){
+void Auto_KF_leg(Leg* leg){
   // take error in state 3
-  if (leg->state == LATE_STANCE) {
-    //    leg->Input is the average of the measured torque
-    //    leg->PID_Stepoint is the reference
-    Serial.print(" Leg Error ");
-    Serial.println(leg->PID_Setpoint - leg->Input );
-    leg->ERR += (leg->PID_Setpoint - leg->Input );
-    leg->count_err++;
+  switch(leg->state){
+  case LATE_STANCE:
+    Auto_KF_leg_Late_stance(Leg* leg);
+    break;
+  case SWING:
+    Auto_KF_leg_Swing(Leg* leg);
+    break;
+  }
+}
+
+void Auto_KF_leg_Late_stance(Leg* leg){
+  Serial.print(" Leg Error ");
+  Serial.println(leg->PID_Setpoint - leg->Input );
+  leg->ERR += (leg->PID_Setpoint - leg->Input );
+  leg->count_err++;
+}
+
+void Auto_KF_leg_Swing(Leg* leg){
+  leg->ERR = leg->ERR / leg->count_err;
+  if ((leg->count_err != 0)) {
+    Serial.print("Leg ERR ");
+    Serial.println(leg->ERR);
+  }
+  leg->count_err = 0;
+
+  if (leg->ERR > max_ERR) {
+    leg->KF += 0.05;
+  }
+  else if (leg->ERR < min_ERR) {
+    leg->KF -= 0.05;
   }
 
-  if (leg->state == SWING) {
-
-    leg->ERR = leg->ERR / leg->count_err;
-    if ((leg->count_err != 0)) {
-      Serial.print("Leg ERR ");
-      Serial.println(leg->ERR);
-    }
-    else {
-
-    }
-    leg->count_err = 0;
-
-
-
-    if (leg->ERR > max_ERR) {
-      leg->KF += 0.05;
-    }
-    else if (leg->ERR < min_ERR) {
-      leg->KF -= 0.05;
-    }
-    else {}
-
-    if (leg->KF >= leg->max_KF)
-      leg->KF = leg->max_KF;
-    else if (leg->KF <= leg->min_KF)
-      leg->KF = leg->min_KF;
-    else {}
-
-    Serial.print("New leg->KF ");
-    Serial.println(leg->KF);
-    leg->ERR = 0;
+  if (leg->KF >= leg->max_KF){
+    leg->KF = leg->max_KF;
+  }else if (leg->KF <= leg->min_KF){
+    leg->KF = leg->min_KF;
   }
 
+  Serial.print("New leg->KF ");
+  Serial.println(leg->KF);
+  leg->ERR = 0;
 }
 
 void Auto_KF() {
