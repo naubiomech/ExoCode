479d4bf939ff583f54e65b7e584ff8028b212741
Jack Garrard <jg2562@nau.edu>
2018-09-22 19:29:17 -0700

    Broke state machine into states

--- State_Machine.ino
+++ State_Machine.ino
@@ -28,84 +28,11 @@ void state_machine_generic(Leg* leg, boolean foot_on_fsr){
   switch (leg->state)
   {
   case SWING: //Swing
-    // This flag enables the "set to zero" procedure for the left ankle.
-    // When you're for at least 3 seconds in the same state, the torque reference is set to zero
-
-    if (leg->set_2_zero == 1) {
-      leg->set_2_zero = 0;
-      leg->One_time_set_2_zero = 1;
-    }
-    else if (foot_on_fsr)
-    {
-      leg->state_count_13++;
-      // if you're in the same state for more than state_counter_th it means that it is not noise
-      if (leg->state_count_13 >= state_counter_th)
-      {
-        leg->sigm_done = true;
-        leg->Old_PID_Setpoint = leg->PID_Setpoint;
-
-        if (abs(leg->Dorsi_Setpoint_Ankle) > 0) {
-          leg->Old_PID_Setpoint = 0;
-        } else {
-          leg->Previous_Dorsi_Setpoint_Ankle = 0;
-        }
-
-        if (leg->Previous_Setpoint_Ankle <= leg->Setpoint_Ankle) {
-
-          leg->New_PID_Setpoint = leg->Previous_Setpoint_Ankle + (leg->Setpoint_Ankle - leg->Previous_Setpoint_Ankle) * leg->coef_in_3_steps;
-
-        } else {
-
-          leg->New_PID_Setpoint = leg->Previous_Setpoint_Ankle - (leg->Previous_Setpoint_Ankle - leg->Setpoint_Ankle) * leg->coef_in_3_steps;
-
-        }
-
-        leg->state_old = leg->state;
-        leg->state = LATE_STANCE;
-        leg->state_count_13 = 0;
-        leg->state_count_31 = 0;
-      }
-    }
-
+    state_machine_swing(leg, foot_on_fsr);
     break;
   case LATE_STANCE: //Late Stance
-
-    if ((leg->set_2_zero == 1) && (leg->One_time_set_2_zero)) {
-      leg->sigm_done = true;
-      leg->Old_PID_Setpoint = leg->PID_Setpoint;
-      leg->state_old = leg->state;
-      leg->New_PID_Setpoint = 0;
-      leg->One_time_set_2_zero = 0;
-      leg->Previous_Setpoint_Ankle = 0;
-      leg->PID_Setpoint = 0;
-      leg->Setpoint_Ankle_Pctrl = 0;
-    }
-
-    if (!foot_on_fsr)
-    {
-      leg->state_count_31++;
-      if (leg->state_count_31 >= state_counter_th)
-      {
-        leg->sigm_done = true;
-        leg->Old_PID_Setpoint = leg->PID_Setpoint;
-        leg->state_old = leg->state;
-
-
-        if (leg->Previous_Dorsi_Setpoint_Ankle <= leg->Dorsi_Setpoint_Ankle) {
-
-          leg->New_PID_Setpoint = leg->Previous_Dorsi_Setpoint_Ankle + (leg->Dorsi_Setpoint_Ankle - leg->Previous_Dorsi_Setpoint_Ankle) * leg->coef_in_3_steps;
-
-        } else {
-
-          leg->New_PID_Setpoint = leg->Previous_Dorsi_Setpoint_Ankle - (leg->Previous_Dorsi_Setpoint_Ankle - leg->Dorsi_Setpoint_Ankle) * leg->coef_in_3_steps;
-
-        }
-
-        leg->state = SWING;
-        leg->state_count_31 = 0;
-        leg->state_count_13 = 0;
-      }
-    }
+    state_machine_late_stance(leg, foot_on_fsr);
+    break;
   }//end switch
   // Adjust the torque reference as a function of the step
   ref_step_adj(leg);
@@ -128,3 +55,82 @@ void state_machine_generic(Leg* leg, boolean foot_on_fsr){
 
 }
 
+void state_machine_swing(Leg* leg, boolean foot_on_fsr){
+  // This flag enables the "set to zero" procedure for the left ankle.
+  // When you're for at least 3 seconds in the same state, the torque reference is set to zero
+
+  if (leg->set_2_zero == 1) {
+    leg->set_2_zero = 0;
+    leg->One_time_set_2_zero = 1;
+  }
+  else if (foot_on_fsr)
+  {
+    leg->state_count_13++;
+    // if you're in the same state for more than state_counter_th it means that it is not noise
+    if (leg->state_count_13 >= state_counter_th)
+    {
+      leg->sigm_done = true;
+      leg->Old_PID_Setpoint = leg->PID_Setpoint;
+
+      if (abs(leg->Dorsi_Setpoint_Ankle) > 0) {
+        leg->Old_PID_Setpoint = 0;
+      } else {
+        leg->Previous_Dorsi_Setpoint_Ankle = 0;
+      }
+
+      if (leg->Previous_Setpoint_Ankle <= leg->Setpoint_Ankle) {
+
+        leg->New_PID_Setpoint = leg->Previous_Setpoint_Ankle + (leg->Setpoint_Ankle - leg->Previous_Setpoint_Ankle) * leg->coef_in_3_steps;
+
+      } else {
+
+        leg->New_PID_Setpoint = leg->Previous_Setpoint_Ankle - (leg->Previous_Setpoint_Ankle - leg->Setpoint_Ankle) * leg->coef_in_3_steps;
+
+      }
+
+      leg->state_old = leg->state;
+      leg->state = LATE_STANCE;
+      leg->state_count_13 = 0;
+      leg->state_count_31 = 0;
+    }
+  }
+}
+
+void state_machine_late_stance(Leg* leg, boolean foot_on_fsr){
+  if ((leg->set_2_zero == 1) && (leg->One_time_set_2_zero)) {
+    leg->sigm_done = true;
+    leg->Old_PID_Setpoint = leg->PID_Setpoint;
+    leg->state_old = leg->state;
+    leg->New_PID_Setpoint = 0;
+    leg->One_time_set_2_zero = 0;
+    leg->Previous_Setpoint_Ankle = 0;
+    leg->PID_Setpoint = 0;
+    leg->Setpoint_Ankle_Pctrl = 0;
+  }
+
+  if (!foot_on_fsr)
+  {
+    leg->state_count_31++;
+    if (leg->state_count_31 >= state_counter_th)
+    {
+      leg->sigm_done = true;
+      leg->Old_PID_Setpoint = leg->PID_Setpoint;
+      leg->state_old = leg->state;
+
+
+      if (leg->Previous_Dorsi_Setpoint_Ankle <= leg->Dorsi_Setpoint_Ankle) {
+
+        leg->New_PID_Setpoint = leg->Previous_Dorsi_Setpoint_Ankle + (leg->Dorsi_Setpoint_Ankle - leg->Previous_Dorsi_Setpoint_Ankle) * leg->coef_in_3_steps;
+
+      } else {
+
+        leg->New_PID_Setpoint = leg->Previous_Dorsi_Setpoint_Ankle - (leg->Previous_Dorsi_Setpoint_Ankle - leg->Dorsi_Setpoint_Ankle) * leg->coef_in_3_steps;
+
+      }
+
+      leg->state = SWING;
+      leg->state_count_31 = 0;
+      leg->state_count_13 = 0;
+    }
+  }
+}
