5022d818d6136ecbcd73e7f29f74d5b57181f542
Jack Garrard <jg2562@nau.edu>
2018-10-03 16:14:36 -0700

    Auto fixed formatting in the newer header files

--- FSR.h
+++ FSR.h
@@ -1,16 +1,16 @@
 
 class FSR{
 public:
-	unsigned int pin;
+  unsigned int pin;
 
   double force;
 
-	double fsr = 0;
-	double fsr_peak_ref = 0;
-	double fsr_percent_thresh = 0.9;
-	int address_FSR;
-	int baseline_address;
-	double baseline_value;
+  double fsr = 0;
+  double fsr_peak_ref = 0;
+  double fsr_percent_thresh = 0.9;
+  int address_FSR;
+  int baseline_address;
+  double baseline_value;
 };
 
 class FSRGroup{
@@ -26,7 +26,7 @@ public:
   double force;
 
   double fsr_Combined_peak_ref;
-	double FSR_Ratio;
-	double Max_FSR_Ratio;
-	int FSR_baseline_FLAG = 0;
+  double FSR_Ratio;
+  double Max_FSR_Ratio;
+  int FSR_baseline_FLAG = 0;
 };
--- Motor.h
+++ Motor.h
@@ -3,10 +3,10 @@
 
 class Motor{
 private:
-	double measureRawTorque();
+  double measureRawTorque();
 public:
-	Motor(int motor_pin, int torque_sensor_pin, int err_pin);
-	double getTorque();
+  Motor(int motor_pin, int torque_sensor_pin, int err_pin);
+  double getTorque();
   void measureTorque();
   void measureError();
   bool hasErrored();
@@ -14,40 +14,40 @@ public:
   bool inErrorState;
 
   unsigned int torque_sensor_pin;
-	unsigned int motor_pin;
-	unsigned int err_pin;
+  unsigned int motor_pin;
+  unsigned int err_pin;
 
-	double sign = 1;
+  double sign = 1;
 
-	double torque_measurements[dim] = {0};
+  double torque_measurements[dim] = {0};
 
-	double averaged_torque;
+  double averaged_torque;
 
-	double PID_Setpoint, Input, Output;
-	PID pid = PID(&Input, &Output, &PID_Setpoint, PID_DEFAULTS[0], PID_DEFAULTS[1], PID_DEFAULTS[2], DIRECT);
+  double PID_Setpoint, Input, Output;
+  PID pid = PID(&Input, &Output, &PID_Setpoint, PID_DEFAULTS[0], PID_DEFAULTS[1], PID_DEFAULTS[2], DIRECT);
 
-	double ERR;
-	double max_KF;
-	double min_KF;
-	int count_err;
+  double ERR;
+  double max_KF;
+  double min_KF;
+  int count_err;
 
-	double KF = 1;
+  double KF = 1;
 
-	double torque_calibration_value = 0;
-	int Vol;
+  double torque_calibration_value = 0;
+  int Vol;
 
-	int torque_address;
+  int torque_address;
 
-	double Setpoint, Setpoint_Pctrl;
-	double Previous_Setpoint = 0;
-	double Setpoint_earlyStance = 0.25 * Setpoint;
-	double Dorsi_Setpoint;
-	double Previous_Dorsi_Setpoint;
+  double Setpoint, Setpoint_Pctrl;
+  double Previous_Setpoint = 0;
+  double Setpoint_earlyStance = 0.25 * Setpoint;
+  double Dorsi_Setpoint;
+  double Previous_Dorsi_Setpoint;
 
-	double New_PID_Setpoint = 0.0;
-	double Old_PID_Setpoint = 0.0;
+  double New_PID_Setpoint = 0.0;
+  double Old_PID_Setpoint = 0.0;
 
-	double zero_torque;
+  double zero_torque;
 };
 
 #endif
