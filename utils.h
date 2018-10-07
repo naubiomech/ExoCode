4e06b0b7deeb7e89544eabbd89d0f15f529162ae
Jack Garrard <jg2562@nau.edu>
2018-10-06 17:00:34 -0700

    Created an averager

--- utils.h
+++ utils.h
@@ -1,6 +1,16 @@
 #ifndef UTILITES_HEADER
 #define UTILITES_HEADER
 
+class Averager{
+private:
+  double avg;
+  double count;
+public:
+  void update(double value);
+  double getAverage();
+  void reset();
+};
+
 double clamp(double val, double min, double max);
 
 #endif
--- utils.ino
+++ utils.ino
@@ -1,4 +1,18 @@
 
+void Averager::update(double value){
+  avg += value;
+  count++;
+}
+
+double Averager::getAverage(){
+  return avg / count;
+}
+
+void Averager::reset(){
+  avg = 0;
+  count = 0;
+}
+
 double clamp(double val, double lower, double upper){
   return min(max(val, lower),upper);
 }
