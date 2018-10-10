e298f6d0c77080605c728a260d5e932d693559af
Jack Garrard <jg2562@nau.edu>
2018-10-10 15:51:38 -0700

    Condensed the state machine

--- State_Machine.ino
+++ State_Machine.ino
@@ -25,58 +25,37 @@ void state_machine(Leg* leg)
 }
 
 void state_machine_generic(Leg* leg, boolean foot_on_fsr){
-  switch (leg->state)
-  {
-  case SWING: //Swing
-    state_machine_swing(leg, foot_on_fsr);
-    break;
-  case LATE_STANCE: //Late Stance
-    state_machine_late_stance(leg, foot_on_fsr);
-    break;
-  }//end switch
-  // Adjust the torque reference as a function of the step
+  if (leg->set_2_zero){
+    switch (leg->state) {
+    case SWING:
+      leg->set_2_zero = 0;
+      leg->One_time_set_2_zero = 1;
+      break;
+    case LATE_STANCE:
+      if ((leg->set_2_zero == 1) && (leg->One_time_set_2_zero)) {
+        leg->sigm_done = true;
+        leg->Old_PID_Setpoint = leg->PID_Setpoint;
+        leg->state_old = leg->state;
+        leg->New_PID_Setpoint = 0;
+        leg->One_time_set_2_zero = 0;
+        leg->Previous_Setpoint_Ankle = 0;
+        leg->PID_Setpoint = 0;
+        leg->Setpoint_Ankle_Pctrl = 0;
+      }
+      break;
+    }
+  }
+
+  leg->determineState(foot_on_fsr);
   ref_step_adj(leg);
 
   if ((Trq_time_volt == 2 || Trq_time_volt == 3) && leg->state == LATE_STANCE) {
     leg->PID_Setpoint = leg->Setpoint_Ankle_Pctrl;
+  } else if (N1 < 1 || N2 < 1 || N3 < 1) {
+    leg->PID_Setpoint = leg->New_PID_Setpoint;
+  } else {
+    // Create the smoothed reference and call the PID
+    PID_Sigm_Curve(leg);
   }
-  else {
-
-    if (N1 < 1 || N2 < 1 || N3 < 1) {
-      leg->PID_Setpoint = leg->New_PID_Setpoint;
-    }
-    else {
-      // Create the smoothed reference and call the PID
-      PID_Sigm_Curve(leg);
-    }
-
-  }
-
-
-}
-
-void state_machine_swing(Leg* leg, boolean foot_on_fsr){
-  // This flag enables the "set to zero" procedure for the left ankle.
-  // When you're for at least 3 seconds in the same state, the torque reference is set to zero
-
-  if (leg->set_2_zero == 1) {
-    leg->set_2_zero = 0;
-    leg->One_time_set_2_zero = 1;
-  }
-  leg->determineState(foot_on_fsr);
-}
-
-void state_machine_late_stance(Leg* leg, boolean foot_on_fsr){
-  if ((leg->set_2_zero == 1) && (leg->One_time_set_2_zero)) {
-    leg->sigm_done = true;
-    leg->Old_PID_Setpoint = leg->PID_Setpoint;
-    leg->state_old = leg->state;
-    leg->New_PID_Setpoint = 0;
-    leg->One_time_set_2_zero = 0;
-    leg->Previous_Setpoint_Ankle = 0;
-    leg->PID_Setpoint = 0;
-    leg->Setpoint_Ankle_Pctrl = 0;
-  }
-  leg->determineState(foot_on_fsr);
 
 }
