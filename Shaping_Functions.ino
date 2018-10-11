0c6c9e326cfa75229a2112d00f7e6da6d74b5246
Jack Garrard <jg2562@nau.edu>
2018-10-10 20:03:44 -0700

    Fixed the shaping functions

--- Shaping_Functions.ino
+++ Shaping_Functions.ino
@@ -3,7 +3,7 @@
 
 
 double calculatePIDSetpointSigm(double New_PID_Setpoint, double Old_PID_Setpoint,
-                                double Ts, double exp_mult, int leg->n_iter, int N)
+                                double Ts, double exp_mult, int n_iter, int N)
 { // Makes the curve for your setpoint vs time look like the voltage time graph for a charging capacitor
   //leg->n_iter tells you at which of the Nth sample you are not counting the zero
   //Ts sampling time in this case 0.001 with exp_mult=2000 it takes 6 milliseconds to rise from 0 to 1
@@ -12,7 +12,7 @@ double calculatePIDSetpointSigm(double New_PID_Setpoint, double Old_PID_Setpoint
   //  if ((N % 2)) {
   //    N++;
   //  }
-  double sig = 1 / (1 + exp(-exp_mult * ((-N / 2 + leg->n_iter + 1)) * Ts));
-  Current_PID_Setpoint = Old_PID_Setpoint + (New_PID_Setpoint - Old_PID_Setpoint) * sig;
+  double sig = 1 / (1 + exp(-exp_mult * ((-1 * N / 2 + n_iter + 1)) * Ts));
+  double Current_PID_Setpoint = Old_PID_Setpoint + (New_PID_Setpoint - Old_PID_Setpoint) * sig;
   return Current_PID_Setpoint;
 }
