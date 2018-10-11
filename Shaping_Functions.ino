821d01d0f62e80c78f0fd33826d58c42d47c1087
Jack Garrard <jg2562@nau.edu>
2018-10-10 17:49:52 -0700

    Updated shaping function in preparation of moving it to methods

--- Shaping_Functions.ino
+++ Shaping_Functions.ino
@@ -2,64 +2,50 @@
 //Calc Sigmoid function and apply to the New point
 
 
-double Change_PID_Setpoint_Sigm(double New_PID_Setpoint_l, double Current_PID_Setpoint, double Old_PID_Setpoint_l, double Ts_l, double exp_mult_l, int n_iter_l, int N_l)
+double calculatePIDSetpointSigm(double New_PID_Setpoint, double Old_PID_Setpoint, double Ts, double exp_mult, int n_iter, int N)
 { // Makes the curve for your setpoint vs time look like the voltage time graph for a charging capacitor
   //n_iter tells you at which of the Nth sample you are not counting the zero
   //Ts sampling time in this case 0.001 with exp_mult=2000 it takes 6 milliseconds to rise from 0 to 1
   // it has to stop if N==n_iter
-  //  N_l = round(1 / (Ts_l * exp_mult_l) * 10);
-  //  if ((N_l % 2)) {
-  //    N_l++;
+  //  N = round(1 / (Ts * exp_mult) * 10);
+  //  if ((N % 2)) {
+  //    N++;
   //  }
-  double sig = 1 / (1 + exp(-exp_mult_l * ((-N_l / 2 + n_iter_l + 1)) * Ts_l));
-  Current_PID_Setpoint = Old_PID_Setpoint_l + (New_PID_Setpoint_l - Old_PID_Setpoint_l) * sig;
+  double sig = 1 / (1 + exp(-exp_mult * ((-N / 2 + n_iter + 1)) * Ts));
+  Current_PID_Setpoint = Old_PID_Setpoint + (New_PID_Setpoint - Old_PID_Setpoint) * sig;
   return Current_PID_Setpoint;
 }
 
 void PID_Sigm_Curve(Leg* leg){
-  leg->sig_time = millis();                                                        //Start time for sig
-  if (leg->sig_time - leg->sig_time_old > 1)
+  long sig_time = millis();                                                        //Start time for sig
+  if ((sig_time - leg->sig_time_old) > 1)
   {
-    leg->sig_time_old = leg->sig_time;                                                  //??? records for next time this code runs??
+    leg->sig_time_old = sig_time;                                                  //??? records for next time this code runs??
 
     if ((abs(leg->New_PID_Setpoint - leg->PID_Setpoint) > 0.1) && (leg->sigm_done))
     { //if state machine transition has occured and the newsetpoint is greater than the setpoint
       leg->sigm_done = false;                                                   //Do not let the code enter this block, uhh until the setpoint transition has finished?
-      leg->n_iter = 0;
+      n_iter = 0;
+      int N_step;
 
       if (leg->state == LATE_STANCE) {
-        leg->N_step = leg->N3;//12 * 10;                                             //Defines number of steps
-      }
-      else if (leg->state == 2) {                                             //So I think this N_step business determines how quickly the PID Setpoint rises or falls
-        leg->N_step = leg->N2;//6 * 10;
-      }
-      else if (leg->state == SWING) {
-        leg->N_step = leg->N1;
+        N_step = N3;//12 * 10;                                             //Defines number of steps
+      } else if (leg->state == SWING) {
+        N_step = N1;
       }
 
-      double exp_mult = round((10 / Ts) / (leg->N_step - 1));                         //???
+      double exp_mult = round((10 / Ts) / (N_step - 1));                         //???
 
     }// end if sigm_done
 
-    if (leg->n_iter < leg->N_step)
+    if (n_iter < N_step)
     {
       // Determines the new intermediate PID Setpoint
-      leg->PID_Setpoint = Change_PID_Setpoint_Sigm(leg->New_PID_Setpoint, leg->PID_Setpoint, leg->Old_PID_Setpoint, Ts, exp_mult, leg->n_iter, leg->N_step);
-      leg->n_iter++;                    //Takes in       goal Setpoint, instantaneous setpoint,   previous setpoint, time interval,    constant, our location along the x axis, length of x axis
-    }
-    if (leg->n_iter >= leg->N_step)
-    {
+      leg->PID_Setpoint = calculatePIDSetpointSigm(New_PID_Setpoint, leg->Old_PID_Setpoint,
+                                                   Ts, exp_mult, n_iter, N_step);
+      n_iter++;
+    } else {
       leg->sigm_done = true;
     }
   }
-}
-
-void PID_Sigm_Curve_RL()
-{
-  PID_Sigm_Curve(right_leg);
-}
-
-void PID_Sigm_Curve_LL()
-{
-  PID_Sigm_Curve(left_leg);
 }
