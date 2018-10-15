64b26f86884ab1b96b2c60bd7b8875764b8ec29a
Jack Garrard <jg2562@nau.edu>
2018-10-14 21:57:18 -0700

    Removed syntax error in shaping functions

--- Shaping_Functions.h
+++ Shaping_Functions.h
@@ -2,5 +2,5 @@
 #define SHAPING_FUNCTIONS_HEADER
 
 double calculatePIDSetpointSigm(double New_PID_Setpoint, double Old_PID_Setpoint,
-								double Ts, double exp_mult, int leg->n_iter, int N);
+								double Ts, double exp_mult, int n_iter, int N);
 #endif
