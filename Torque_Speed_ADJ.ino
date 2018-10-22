6d67e9fcb8f92745b4db058f245e9c508062c573
Jack Garrard <jg2562@nau.edu>
2018-10-21 19:35:05 -0700

    Further refactored torque speed adj

--- Torque_Speed_ADJ.ino
+++ Torque_Speed_ADJ.ino
@@ -62,7 +62,7 @@ int take_baseline_dorsi(Steps* p_steps, int* p_flag_take_baseline){
     // calculate plantarflexion
     p_steps->plant_time = millis() - (p_steps->plant_time);
 
-    if (p_steps->plant_time <= step_timeength)
+    if (p_steps->plant_time <= step_time_length)
     {
       p_steps->flag_start_plant = false;
       return 0;
@@ -103,238 +103,176 @@ int take_baseline(int state, int state_old, Steps* p_steps, int* p_flag_take_bas
   return 0;
 }
 
+double clamp_setpoint(double raw_setpoint, Clamp* setpoint_clamp){
+  if(raw_setpoint == 0){
+    return 0;
+  }
+
+  double setpoint_sign = abs(raw_setpoint) / raw_setpoint;
+  double setpoint = setpoint_sign * setpoint_clamp->clamp(abs(raw_setpoint));
+  return setpoint;
+}
+
+double Ctrl_ADJ_plantar(){
+  // update the voltage peak to update torque in case of Bang Bang ctrl
+  if (p_steps->curr_voltage > p_steps->peak)
+    p_steps->peak =  p_steps->curr_voltage;
+
+  *p_FSRatio = fabs(p_steps->curr_voltage / p_steps->plant_peak_mean);
+  if (*p_FSRatio > (*p_Max_FSRatio))
+    (*p_Max_FSRatio) = *p_FSRatio;
 
 
 
-double Ctrl_ADJ(int R_state_l, int R_state_old_l, Steps* p_steps_l, double N3_l, double New_PID_Setpoint_l,
-                double* p_Setpoint_Ankle_l, double * p_Setpoint_Ankle_Pctrl_l, int flag_torque_time_volt_l,
-                double prop_gain_l, double taking_baseline_l, double *p_FSR_Ratio, double* p_Max_FSR_Ratio) {
+  if (flag_torque_time_volt != 0){
+    double new_setpoint;
+    if (flag_torque_time_volt == 2) {
+      new_setpoint = (p_steps->Setpoint ) * (prop_gain) * (*p_FSRatio);
 
-  // Speed adjustment -> the smoothing parameters are updated as a function of the plantar time in order to modify the shaping of the torque to the new step time.
-  // It considers the average time of 4 steps. There's a filter of step_time_length on the plantarflexion time in order to cut the noise
-  // The faster you go the more N3 decreases
+    } else if (flag_torque_time_volt == 3) {
+      // Second version of pivot proportional Ctrl with polynomial law XXXXX QUI METTERE LEGGE
+      new_setpoint =(p_steps->Setpoint ) *
+        (p_prop[0] * pow(*p_FSRatio, 2) + p_prop[1] * (*p_FSRatio) + p_prop[2]) / (p_prop[0] + p_prop[1] + p_prop[2]) ;
 
-  // Torque adjustment -> the torque set point is modified as a function of the voltage measured , the faster you go probably the bigger you hit the floor and hence
-  // the voltage increases. It modifies the setpoint at each step.
+    } else if (flag_torque_time_volt == 1) {
 
-  if (taking_baseline_l) {
-    *p_Setpoint_Ankle_Pctrl_l = 0;
-    *p_Setpoint_Ankle_l = 0;
-    return N3_l;
+      new_setpoint = (p_steps->Setpoint ) *
+        (p_prop[0] * pow((*p_Max_FSRatio), 2) + p_prop[1] * (*p_Max_FSRatio) + p_prop[2]) / (p_prop[0] + p_prop[1] + p_prop[2]);
+
+      N3 = 1;
+    }
+    new_setpoint = clamp_setpoint(new_setpoint, p_steps->setpoint_clamp);
+    *p_Setpoint_Ankle_Pctrl = new_setpoint;
+    return N3;
+  }
+
+  // Otherwise we need to calculate the time
+
+  // Parameters for speed adaption
+  if (p_steps->flag_start_plant == false) // if it is true it means you started the step. Here I inizialize the parameters for speed adaption.
+  {
+    p_steps->plant_time = millis(); // start the plantarflexion
+    p_steps->dorsi_time = millis() - (p_steps->dorsi_time); // calculate the dorsiflexion that has just finished
+
+    if (p_steps->dorsi_time <= step_time_length / 4) // if <50ms probably it is noise
+    {
+      p_steps->peak = 0;
+      p_steps->flag_start_plant = false;
+      return N3;
+    }
+
+    p_steps->flag_start_plant = true; // Parameters inizialized Start a step
+  }
+
+
+  // Torque adaption as a function of the speed or of the pressure force
+  // If you want to adjust the torque and hence torque_adj = 1
+  //-----------------add 11:49 6/19/18
+  if (p_steps->torque_adj)
+  {
+    if (p_steps->plant_time <= step_time_length)
+    {
+      p_steps->peak = 0;
+      p_steps->flag_start_plant = false;
+      Serial.println(" TRQ ADJ plant time too short ");
+      return N3;
+    }
+
+
+    // if you use plantar time as reference to increase the torque
+    double new_setpoint;
+    if (p_steps->flag_start_plant == true) {
+      if (flag_torque_time_volt == 0) {
+        // if you're going use the time as reference to increase also the torque
+        new_setpoint = (p_steps->Setpoint ) * (1 / (fabs(p_steps->plant_mean / p_steps->plant_mean_base)));
+      } else if (flag_torque_time_volt == 1) { // If you use the volt or force value returned by the FSR sensors
+        new_setpoint = (p_steps->Setpoint ) * (fabs(p_steps->peak / p_steps->plant_peak_mean));
+      }
+      new_setpoint = clamp_setpoint(new_setpoint, p_steps->setpoint_clamp);
+      *p_Setpoint_Ankle = new_setpoint;
+    }
+  }// end torque adj
+  return N3;
+
+}
+
+double Ctrl_ADJ_dorsi(){
+
+  if (p_steps->flag_start_plant) { // If a step has started i.e. the states have passed from 1 or 2 to 3
+    // if you transit from 3 to 1 plantar flexion is completed and start dorsiflexion
+
+    p_steps->count_plant++; // you have accomplished a step
+
+    // start dorsiflexion
+    p_steps->dorsi_time = millis();
+
+    if (p_steps->flag_N3_adjustment_time) { // If you wanted to adjust the smoothing as a function of the speed
+      // check for the first step in order to see if everything started properly
+      if (p_steps->n_steps == 1) {
+        Serial.println(" N3 Adj activated");
+      }
+      p_steps->n_steps++;
+    }
+
+    // calculate plantarflexion
+    p_steps->plant_time = millis() - (p_steps->plant_time);
+
+    if (p_steps->plant_time <= step_time_length) {
+      p_steps->flag_start_plant = false;
+      return N3;
+    }
+
+    p_steps->flag_start_plant = false; // you have provided one step
+
+    if (p_steps->count_plant >= 2) {
+      // this is the time window of the filter for the plantarflexion
+      p_steps->plant_mean =
+        take_baseline_get_mean_time(p_steps->plant_time, p_steps->plant_mean,
+                                    p_steps, p_steps->plant_time_averager);
+
+      // Update mean value
+      p_steps->plant_mean = p_steps->plant_time_averager->getAverage();
+    }
+
+    if (p_steps->flag_N3_adjustment_time) {
+      N3 = round((p_steps->plant_mean) * p_steps->perc);
+      N3 = min(500, max(4, N3));
+    }
+  }// end if flag_start_plant
+  return N3;
+}
+
+double Ctrl_ADJ(int state, int state_old, Steps* p_steps, double N3, double New_PID_Setpoint,
+                double* p_Setpoint_Ankle, double * p_Setpoint_Ankle_Pctrl, int flag_torque_time_volt,
+                double prop_gain, double taking_baseline, double *p_FSRatio, double* p_Max_FSRatio) {
+
+  // Speed adjustment -> the smoothing parameters are updated as a function of the plantar time in order to modify the
+  // shaping of the torque to the new step time.
+
+  // It considers the average time of 4 steps. There's a filter of step_time_length on the plantarflexion time in order
+  // to cut the noise the faster you go the more N3 decreases
+
+  // Torque adjustment -> the torque set point is modified as a function of the voltage measured ,
+  // the faster you go probably the bigger you hit the floor and hence the voltage increases.
+  //It modifies the setpoint at each step.
+
+  if (taking_baseline) {
+    *p_Setpoint_Ankle_Pctrl = 0;
+    *p_Setpoint_Ankle = 0;
+    return N3;
   }
 
   // if you transit from state 1 to state 3 dorsiflexion is completed and start plantarflexion
-  if ((R_state_l == 3) && (R_state_old_l == 1 || R_state_old_l == 2))
-  {
-
-
-    // update the voltage peak to update torque in case of Bang Bang ctrl
-    if (p_steps_l->curr_voltage > p_steps_l->peak)
-      p_steps_l->peak =  p_steps_l->curr_voltage;
-
-    *p_FSR_Ratio = fabs(p_steps_l->curr_voltage / p_steps_l->plant_peak_mean);
-    if (*p_FSR_Ratio > (*p_Max_FSR_Ratio))
-      (*p_Max_FSR_Ratio) = *p_FSR_Ratio;
-
-
-
-    if (flag_torque_time_volt_l == 2) {
-      if ((p_steps_l->Setpoint ) > 0) {
-        *p_Setpoint_Ankle_Pctrl_l = max(Min_Prop, (p_steps_l->Setpoint ) * (prop_gain_l) * (*p_FSR_Ratio)); // the difference here is that we do it as a function of the FSR calibration
-        *p_Setpoint_Ankle_Pctrl_l = min(Max_Prop, *p_Setpoint_Ankle_Pctrl_l);
-      }
-      else if ((p_steps_l->Setpoint ) < 0) {
-        *p_Setpoint_Ankle_Pctrl_l = max(-Max_Prop, (p_steps_l->Setpoint ) * (prop_gain_l) * (*p_FSR_Ratio)); // the difference here is that we do it as a function of the FSR calibration
-        *p_Setpoint_Ankle_Pctrl_l = min(Min_Prop, *p_Setpoint_Ankle_Pctrl_l);
-      } else {
-        *p_Setpoint_Ankle_Pctrl_l = 0;
-      }
-      return N3_l; // No modification in the shaping
-
-    } else if (flag_torque_time_volt_l == 3) {
-      // Second version of pivot proportional Ctrl with polynomial law XXXXX QUI METTERE LEGGE
-      if ((p_steps_l->Setpoint ) > 0) {
-        *p_Setpoint_Ankle_Pctrl_l = max(Min_Prop, (p_steps_l->Setpoint ) * (p_prop[0] * pow(*p_FSR_Ratio, 2) + p_prop[1] * (*p_FSR_Ratio) + p_prop[2]) / (p_prop[0] + p_prop[1] + p_prop[2])); // the difference here is that we do it as a function of the FSR calibration
-        *p_Setpoint_Ankle_Pctrl_l = min(Max_Prop, *p_Setpoint_Ankle_Pctrl_l);
-      }
-      else if ((p_steps_l->Setpoint ) < 0) {
-        *p_Setpoint_Ankle_Pctrl_l = max(-Max_Prop, (p_steps_l->Setpoint ) * (p_prop[0] * pow(*p_FSR_Ratio, 2) + p_prop[1] * (*p_FSR_Ratio) + p_prop[2]) / (p_prop[0] + p_prop[1] + p_prop[2])); // the difference here is that we do it as a function of the FSR calibration
-        *p_Setpoint_Ankle_Pctrl_l = min(Min_Prop, *p_Setpoint_Ankle_Pctrl_l);
-      } else {
-        *p_Setpoint_Ankle_Pctrl_l = 0;
-      }
-
-      return N3_l; // No modification in the shaping
-
-    } else if (flag_torque_time_volt_l == 1) {
-
-
-      Serial.print(" Ratios : ");
-      Serial.print(*p_FSR_Ratio);
-      Serial.print(" , ");
-      Serial.print((*p_Max_FSR_Ratio));
-      Serial.println();
-
-      if ((p_steps_l->Setpoint ) > 0) {
-        *p_Setpoint_Ankle_l = max(Min_Prop, (p_steps_l->Setpoint ) * (p_prop[0] * pow((*p_Max_FSR_Ratio), 2) + p_prop[1] * (*p_Max_FSR_Ratio) + p_prop[2]) / (p_prop[0] + p_prop[1] + p_prop[2])); // the difference here is that we do it as a function of the FSR calibration
-        *p_Setpoint_Ankle_l = min(Max_Prop, *p_Setpoint_Ankle_l);
-      }
-      else if ((p_steps_l->Setpoint ) < 0) {
-        *p_Setpoint_Ankle_l = max(-Max_Prop, (p_steps_l->Setpoint ) * (p_prop[0] * pow((*p_Max_FSR_Ratio), 2) + p_prop[1] * (*p_Max_FSR_Ratio) + p_prop[2]) / (p_prop[0] + p_prop[1] + p_prop[2])); // the difference here is that we do it as a function of the FSR calibration
-        *p_Setpoint_Ankle_l = min(Min_Prop, *p_Setpoint_Ankle_l);
-      } else {
-        *p_Setpoint_Ankle_l = 0;
-      }
-      return 1;
-
-    }
-
-    // Otherwise we need to calculate the time
-
-    // Parameters for speed adaption
-    if (p_steps_l->flag_start_plant == false) // if it is true it means you started the step. Here I inizialize the parameters for speed adaption.
-    {
-      p_steps_l->plant_time = millis(); // start the plantarflexion
-      p_steps_l->dorsi_time = millis() - (p_steps_l->dorsi_time); // calculate the dorsiflexion that has just finished
-
-      if (p_steps_l->dorsi_time <= step_time_length / 4) // if <50ms probably it is noise
-      {
-        p_steps_l->peak = 0;
-        p_steps_l->flag_start_plant = false;
-        Serial.println(" SPD ADJ dorsi time too short ");
-        return N3_l;
-      }
-
-      p_steps_l->flag_start_plant = true; // Parameters inizialized Start a step
-    }
-
-
-    // Torque adaption as a function of the speed or of the pressure force
-    // If you want to adjust the torque and hence torque_adj = 1
-    //-----------------add 11:49 6/19/18
-    if (p_steps_l->torque_adj)
-    {
-      if (p_steps_l->plant_time <= step_time_length)
-      {
-        p_steps_l->peak = 0;
-        p_steps_l->flag_start_plant = false;
-        Serial.println(" TRQ ADJ plant time too short ");
-        return N3_l;
-      }
-
-
-      // if you use plantar time as reference to increase the torque
-      if (flag_torque_time_volt_l == 0)
-      {
-        if (p_steps_l->flag_start_plant == true) {
-          // if you're going use the time as reference to increase also the torque
-
-          if ((p_steps_l->Setpoint ) > 0) {
-            *p_Setpoint_Ankle_l = max(Min_Prop, (p_steps_l->Setpoint ) * (1 / (fabs(p_steps_l->plant_mean / p_steps_l->plant_mean_base))));
-            *p_Setpoint_Ankle_l = min(Max_Prop, *p_Setpoint_Ankle_l);
-          }
-          else if ((p_steps_l->Setpoint ) < 0) {
-            *p_Setpoint_Ankle_l = max(-Max_Prop, (p_steps_l->Setpoint ) * (1 / (fabs(p_steps_l->plant_mean / p_steps_l->plant_mean_base))));
-            *p_Setpoint_Ankle_l = min(Min_Prop, *p_Setpoint_Ankle_l);
-          } else {
-            *p_Setpoint_Ankle_Pctrl_l = 0;
-          }
-        }
-      } else if (flag_torque_time_volt_l == 1) // If you use the volt or force value returned by the FSR sensors
-      {
-        if (p_steps_l->flag_start_plant == true) {
-          *p_Setpoint_Ankle_l = (p_steps_l->Setpoint ) * (fabs(p_steps_l->peak / p_steps_l->plant_peak_mean));
-
-          if ((p_steps_l->Setpoint ) > 0) {
-            *p_Setpoint_Ankle_l = max(Min_Prop, (p_steps_l->Setpoint ) * (fabs(p_steps_l->peak / p_steps_l->plant_peak_mean)));
-            *p_Setpoint_Ankle_l = min(Max_Prop, *p_Setpoint_Ankle_l);
-          }
-          else if ((p_steps_l->Setpoint ) < 0) {
-            *p_Setpoint_Ankle_l = max(-Max_Prop, (p_steps_l->Setpoint ) * (fabs(p_steps_l->peak / p_steps_l->plant_peak_mean)));
-            *p_Setpoint_Ankle_l = min(Min_Prop, *p_Setpoint_Ankle_l);
-          } else {
-            *p_Setpoint_Ankle_Pctrl_l = 0;
-          }
-
-        }
-      }
-    }// end torque adj
-
-  }// end if you enter in state 3 from state 2 or 1
-
-  // Hence here I am in state 2 or 1
-
-  if (p_steps_l->flag_start_plant) { // If a step has started i.e. the states have passed from 1 or 2 to 3
-    // if you transit from 3 to 1 plantar flexion is completed and start dorsiflexion
-
-    if ((R_state_old_l == 3) && (R_state_l == 1 || R_state_l == 2)) {
-
-      p_steps_l->count_plant++; // you have accomplished a step
-
-      // start dorsiflexion
-      p_steps_l->dorsi_time = millis();
-
-      if (p_steps_l->flag_N3_adjustment_time) { // If you wanted to adjust the smoothing as a function of the speed
-        // check for the first step in order to see if everything started properly
-        if (p_steps_l->n_steps == 1) {
-          Serial.println(" N3 Adj activated");
-        }
-        p_steps_l->n_steps++;
-      }
-
-      // calculate plantarflexion
-      p_steps_l->plant_time = millis() - (p_steps_l->plant_time);
-
-      if (p_steps_l->plant_time <= step_time_length)
-      {
-
-        p_steps_l->flag_start_plant = false;
-        Serial.println(" SPD ADJ plant time too short ");
-        return N3_l;
-      }
-
-      p_steps_l->flag_start_plant = false; // you have provided one step
-
-      Serial.print(" Plant Time = ");
-      Serial.println(p_steps_l->plant_time);
-
-      if (p_steps_l->count_plant >= 2) {
-        // this is the time window of the filter for the plantarflexion
-        if (p_steps_l->count_plant - 2 >= n_step_baseline) {
-          for (int i = 0; i < n_step_baseline - 1; i++)
-          {
-            p_steps_l->four_step_plant_time[i] = p_steps_l->four_step_plant_time[i + 1];
-          }
-          p_steps_l->four_step_plant_time[n_step_baseline - 1] = p_steps_l->plant_time;
-        }
-        else {
-          p_steps_l->four_step_plant_time[p_steps_l->count_plant - 2] = p_steps_l->plant_time;
-        }
-
-        // Update mean value
-        p_steps_l->plant_mean = 0;
-        for (int i = 0; i < n_step_baseline; i++) {
-          p_steps_l->plant_mean += p_steps_l->four_step_plant_time[i];
-        }
-
-        p_steps_l->plant_mean = p_steps_l->plant_mean / n_step_baseline;
-      }
-
-      if (p_steps_l->flag_N3_adjustment_time) {
-        N3_l = round((p_steps_l->plant_mean) * p_steps_l->perc_l);
-        if (N3_l <= 4) N3_l = 4;
-        if (N3_l >= 500) N3_l = 500;
-      }
-
-
-    }//end if R old 3 i.e. finish plantarflexion
-  }// end if flag_start_plant
-
-  // During the all dorsiflexion set the voltage peak to 0, probably we just need to do it one time
-  if (((R_state_l == 1) || (R_state_l == 2)) && R_state_old_l == 3){
-    p_steps_l->peak = 0;
-    p_Max_FSR_Ratio = 0;
+  if ((state == LATE_STANCE) && (state_old == SWING)) {
+    N3 = Ctrl_ADJ_plantar();
   }
 
-  return N3_l;
+  if ((state == SWING) && (state_old == LATE_STANCE)) {
+    N3 = Ctrl_ADJ_dorsi();
+    // During the all dorsiflexion set the voltage peak to 0, probably we just need to do it one time
+    p_steps->peak = 0;
+    p_Max_FSRatio = 0;
+  }
+
+  return N3;
 }
