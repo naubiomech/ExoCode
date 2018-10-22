dce63c2f1268f4983ff41f8ea51bd394f5d582ff
Jack Garrard <jg2562@nau.edu>
2018-10-21 17:57:21 -0700

    Added a fixed average class

--- Utils.h
+++ Utils.h
@@ -11,24 +11,35 @@ public:
   void reset();
 };
 
+class FixedAverage{
+private:
+	double* previous_values;
+	double average;
+	int size;
+public:
+	FixedAverage(int size);
+	double updateAverage(double value);
+	double getAverage();
+};
+
 class Clamp{
 private:
-  double upper;
-  double lower;
+	double upper;
+	double lower;
 public:
-  Clamp(double upper, double lower);
-  double clamp(double value);
+	Clamp(double upper, double lower);
+	double clamp(double value);
 };
 
 class Threshold{
 private:
-  double upper_threshold;
-  int crossed_threshold_max;
-  int crossed_threshold_count;
-  bool state;
+	double upper_threshold;
+	int crossed_threshold_max;
+	int crossed_threshold_count;
+	bool state;
 public:
-  Threshold(bool starting_state, double threshold_value, int crossed_threshold_max);
-  bool getState(double value);
+	Threshold(bool starting_state, double threshold_value, int crossed_threshold_max);
+	bool getState(double value);
 };
 
 void updateMax(double* max_val, double val);
--- Utils.ino
+++ Utils.ino
@@ -14,6 +14,25 @@ void Average::reset(){
   count = 0;
 }
 
+FixedAverage::FixedAverage(int size){
+  previous_values = new double[size];
+}
+
+double FixedAverage::updateAverage(double value){
+  average = value;
+  for(int i = 0; i < size-1;i++){
+    average += previous_values[i];
+    previous_values[i+1] = previous_values[i];
+  }
+  previous_values[0] = value;
+  average /= size;
+  return average;
+}
+
+double FixedAverage::getAverage(){
+  return average;
+}
+
 Clamp::Clamp(double upper, double lower){
   this->upper = upper;
   this->lower = lower;
@@ -23,30 +42,30 @@ double Clamp::clamp(double val){
   return min(max(val, lower),upper);
 }
 
-void updateMax(double* max_value, double value){
-  if (*max_value > value) {
-    *max_value = value;
-  }
-}
-
-Threshold::Threshold(bool starting_state, double threshold_value, int crossed_threshold_max){
-  this->upper_threshold = threshold_value;
-  this->crossed_threshold_max = crossed_threshold_max;
-  this->state = starting_state;
-  this->crossed_threshold_count = 0;
-}
-
-bool Threshold::getState(double value){
-  if((value > upper_threshold) == state){
-    crossed_threshold_count = 0;
-  } else {
-    crossed_threshold_count++;
+  void updateMax(double* max_value, double value){
+    if (*max_value > value) {
+      *max_value = value;
+    }
   }
 
-  if(crossed_threshold_count == crossed_threshold_max){
-    state = !state;
-    crossed_threshold_count = 0;
+  Threshold::Threshold(bool starting_state, double threshold_value, int crossed_threshold_max){
+    this->upper_threshold = threshold_value;
+    this->crossed_threshold_max = crossed_threshold_max;
+    this->state = starting_state;
+    this->crossed_threshold_count = 0;
   }
 
-  return state;
-}
+  bool Threshold::getState(double value){
+    if((value > upper_threshold) == state){
+      crossed_threshold_count = 0;
+    } else {
+      crossed_threshold_count++;
+    }
+
+    if(crossed_threshold_count == crossed_threshold_max){
+      state = !state;
+      crossed_threshold_count = 0;
+    }
+
+    return state;
+  }
