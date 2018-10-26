667f1b7b15681719a5ee3e634fe340b08f654450
Jack Garrard <jg2562@nau.edu>
2018-10-25 17:45:39 -0700

    Reorganized the variables in the step struct

--- Steps.h
+++ Steps.h
@@ -4,31 +4,41 @@
 
 class Steps{
 public:
-  int    n_steps;
-  double dorsi_time;
-  double plant_time;
-
+  // Leg
   FixedAverage* plant_time_averager = new FixedAverage(n_step_baseline);
   FixedAverage* peak_time_averager = new FixedAverage(n_step_baseline);
   FixedAverage* dorsi_time_averager = new FixedAverage(n_step_baseline);
-  Clamp* setpoint_clamp = new Clamp(Min_Prop, Max_Prop);
-  bool flag_take_average = false;
-  bool flag_N3_adjustment_time = false;
+
+  double dorsi_time;
+  double plant_time;
+
   double dorsi_mean;
   double plant_mean;
   double plant_peak_mean;
+
   int count_plant;
   int count_plant_base;
+
   bool flag_start_plant = false;
-  bool flag_take_baseline = false;
+  // Motor
+  Clamp* setpoint_clamp = new Clamp(Min_Prop, Max_Prop);
+  bool flag_N3_adjustment_time = false;
   bool torque_adj = false;
-  double Setpoint;
-  double plant_mean_base;
-  double voltage_peak_ref;
-  double curr_voltage;
+  double Setpoint; // Desired setpoint
   double perc_l = 0.5;
+  // FSR
+  double curr_voltage;
   double fsr_percent_thresh_Toe = 0.9;
   double peak;
+  // Other
+  // Maybe delete
+  int n_steps;
+  bool flag_take_average = false;
+  bool flag_take_baseline = false;
+  double plant_mean_base;
+  double voltage_peak_ref;
+  // Unsorted
+
 };
 
 #endif
