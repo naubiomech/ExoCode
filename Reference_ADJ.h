c88deb340de6ae05ad5a0d3a6da71a232c38d303
Jack Austin Garrard <jg2562@nau.edu>
2018-11-01 15:20:59 -0700

    Fixed formatting of all files

--- Reference_ADJ.h
+++ Reference_ADJ.h
@@ -3,50 +3,50 @@
 #define REFERENCE_ADJ_HEADER
 
 void ref_step_adj(Leg* leg) {
-	if (leg->activate_in_3_steps == 1) {
+  if (leg->activate_in_3_steps == 1) {
 
-		if (leg->first_step == 1) {
-			leg->coef_in_3_steps = 0;
-			leg->first_step = 0;
-		}
+    if (leg->first_step == 1) {
+      leg->coef_in_3_steps = 0;
+      leg->first_step = 0;
+    }
 
-		if ((leg->state == 3) && (leg->state_old == 1) && (leg->start_step == 0)) {
-			leg->start_step = 1;
-			leg->start_time = millis();
-		}
+    if ((leg->state == 3) && (leg->state_old == 1) && (leg->start_step == 0)) {
+      leg->start_step = 1;
+      leg->start_time = millis();
+    }
 
-		if (leg->start_step == 1) {
-			if ((leg->state == 1) && (leg->state_old == 3)) {
-				leg->start_step = 0;
-				if (millis() - leg->start_time >= step_time_length) { // if the transition from 3 to 1 lasted more than 0.3 sec it was a step
-					leg->num_3_steps += 1;
+    if (leg->start_step == 1) {
+      if ((leg->state == 1) && (leg->state_old == 3)) {
+        leg->start_step = 0;
+        if (millis() - leg->start_time >= step_time_length) { // if the transition from 3 to 1 lasted more than 0.3 sec it was a step
+          leg->num_3_steps += 1;
 
-					Serial.println(leg->coef_in_3_steps);
-				}
-			}
-		}
+          Serial.println(leg->coef_in_3_steps);
+        }
+      }
+    }
 
-		leg->coef_in_3_steps = leg->num_3_steps / 6;
-
-
-		if (leg->coef_in_3_steps >= 1) {
-			leg->coef_in_3_steps = 1;
-			leg->activate_in_3_steps = 0;
-			leg->first_step = 1;
-			leg->num_3_steps = 0;
-			leg->start_step = 0;
-		}
+    leg->coef_in_3_steps = leg->num_3_steps / 6;
 
 
-	}
-	return;
+    if (leg->coef_in_3_steps >= 1) {
+      leg->coef_in_3_steps = 1;
+      leg->activate_in_3_steps = 0;
+      leg->first_step = 1;
+      leg->num_3_steps = 0;
+      leg->start_step = 0;
+    }
+
+
+  }
+  return;
 }
 
 void R_ref_step_adj() {
-	ref_step_adj(right_leg);
+  ref_step_adj(right_leg);
 }
 
 void L_ref_step_adj() {
-	ref_step_adj(left_leg);
+  ref_step_adj(left_leg);
 }
 #endif
