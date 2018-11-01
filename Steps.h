ac2cff1011df4641c092725fdc67deffe1c51a05
Jack Austin Garrard <jg2562@nau.edu>
2018-11-01 14:01:39 -0700

    Split the struct and applied that split to variables

--- Steps.h
+++ Steps.h
@@ -2,11 +2,9 @@
 #define STEPS_HEADER
 #include "Parameters.h"
 
-class Steps{
-public:
+struct Leg_Steps{
   // Leg
   FixedAverage* plant_time_averager = new FixedAverage(n_step_baseline);
-  FixedAverage* peak_time_averager = new FixedAverage(n_step_baseline);
 
   double dorsi_time;
   double plant_time;
@@ -18,16 +16,23 @@ public:
   int count_plant_base;
 
   bool flag_start_plant = false;
+}
+
+struct Motor_Steps{
   // Motor
   Clamp* setpoint_clamp = new Clamp(Min_Prop, Max_Prop);
   bool flag_N3_adjustment_time = false;
   bool torque_adj = false;
   double desired_setpoint;
   double perc_l = 0.5;
+}
+
+struct FSR_Steps{
   // FSR
+  FixedAverage* plant_peak_fsr_averager = new FixedAverage(n_step_baseline);
   double curr_voltage;
   double fsr_percent_thresh_Toe = 0.9;
-  double peak;
-};
+  double peak_voltage;
+}
 
 #endif
--- Torque_Speed_ADJ.ino
+++ Torque_Speed_ADJ.ino
@@ -3,27 +3,27 @@
 
 int take_baseline_plantar(Steps* p_steps){
 
-  if (p_steps->flag_start_plant == false) // if it is true it means you started the step. Here I inizialize the parameters for speed adaption.
+  if (leg_steps->flag_start_plant == false) // if it is true it means you started the step. Here I inizialize the parameters for speed adaption.
   {
-    p_steps->plant_time = millis(); // start the plantarflexion
-    p_steps->dorsi_time = millis() - (p_steps->dorsi_time); // calculate the dorsiflexion that has just finished
+    leg_steps->plant_time = millis(); // start the plantarflexion
+    leg_steps->dorsi_time = millis() - (leg_steps->dorsi_time); // calculate the dorsiflexion that has just finished
 
-    if (p_steps->dorsi_time <= step_timelength / 4) // if <50ms probably it is noise
+    if (leg_steps->dorsi_time <= step_timelength / 4) // if <50ms probably it is noise
     {
-      p_steps->peak = 0;
-      p_steps->flag_start_plant = false;
+      FSR_steps->peak_voltage = 0;
+      leg_steps->flag_start_plant = false;
       Serial.println(" BASE Dorsi too short");
     } else {
-      p_steps->flag_start_plant = true; // Parameters inizialized Start a step
+      leg_steps->flag_start_plant = true; // Parameters inizialized Start a step
       Serial.println(" BASE Start Plantar");
     }
   }
   return 0;
 }
 
-double take_baseline_get_mean_time(double time, double current_mean, Steps* p_steps, FixedAverage* averager){
+double take_baseline_get_mean(double time, double current_mean, Steps* p_steps, FixedAverage* averager){
   double mean = current_mean;
-  if (((p_steps->count_plant_base) - 2) >= n_step_baseline) {
+  if (((leg_steps->count_plant_base) - 2) >= n_step_baseline) {
     mean = averager->updateAverage(time);
   } else {
     averager->updateAverage(time);
@@ -32,51 +32,51 @@ double take_baseline_get_mean_time(double time, double current_mean, Steps* p_st
   return mean;
 }
 
-int take_baseline_update_mean_time(Steps* p_steps){
+int take_baseline_update_means(Steps* p_steps){
 
-  p_steps->plant_mean =
-    take_baseline_get_mean_time(p_steps->plant_time, p_steps->plant_mean,
-                                p_steps, p_steps->plant_time_averager);
+  leg_steps->plant_mean =
+    take_baseline_get_mean(leg_steps->plant_time, leg_steps->plant_mean,
+                           p_steps, leg_steps->plant_time_averager);
 
-  p_steps->plant_peak_mean = 0.9 *
-    take_baseline_get_mean_time(p_steps->plant_peak_time, p_steps->plant_peak_mean,
-                                p_steps, p_steps->plant_peak_time_averager);
+  FSR_steps->plant_peak_mean = 0.9 *
+    take_baseline_get_mean(p_steps->plant_peak_fsr, FSR_steps->plant_peak_mean,
+                           p_steps, leg_steps->plant_peak_fsr_averager);
 }
 
 int take_baseline_dorsi(Steps* p_steps){
-  if (p_steps->flag_start_plant) {
+  if (leg_steps->flag_start_plant) {
     // If a step has started i.e. the states have passed from 1 or 2 to 3
     // if you transit from 3 to 1 plantar flexion is completed and start dorsiflexion
 
     // start dorsiflexion
 
-    p_steps->dorsi_time = millis();
+    leg_steps->dorsi_time = millis();
     // calculate plantarflexion
-    p_steps->plant_time = millis() - (p_steps->plant_time);
+    leg_steps->plant_time = millis() - (leg_steps->plant_time);
 
-    if (p_steps->plant_time <= step_time_length)
+    if (leg_steps->plant_time <= step_time_length)
     {
-      p_steps->flag_start_plant = false;
+      leg_steps->flag_start_plant = false;
       return 0;
     } else {
-      p_steps->flag_start_plant = false; // you have provided one plant
-      p_steps->count_plant_base++; // you have accomplished a step
+      leg_steps->flag_start_plant = false; // you have provided one plant
+      leg_steps->count_plant_base++; // you have accomplished a step
     }
 
-    if ((p_steps->count_plant_base) >= 2) { // avoid the first step just to be sure
+    if ((leg_steps->count_plant_base) >= 2) { // avoid the first step just to be sure
 
       take_baseline_update_mean_time();
 
-      if (((p_steps->count_plant_base) - 2) >= n_step_baseline) {
-        (p_steps->count_plant_base) = 0;
-        return (p_steps->count_plant_base);
+      if (((leg_steps->count_plant_base) - 2) >= n_step_baseline) {
+        (leg_steps->count_plant_base) = 0;
+        return (leg_steps->count_plant_base);
 
       } // return 1 activate a flag that stops the calc of the baseline
     }// end if count_plant>2
 
   }
 
-  p_steps->peak = 0;
+  FSR_steps->peak_voltage = 0;
   return 0;
 }
 
@@ -85,8 +85,8 @@ int take_baseline(int state, int state_old, Steps* p_steps) {
 
   if ((state == LATE_STANCE) && state_old == SWING) { // I am in plantarflexion
     // update the voltage peak
-    if (p_steps->curr_voltage > p_steps->peak)
-      p_steps->peak =  p_steps->curr_voltage;
+    if (FSR_steps->curr_voltage > FSR_steps->peak_voltage)
+      FSR_steps->peak_voltage =  FSR_steps->curr_voltage;
 
     return take_baseline_plant(p_steps);
   }
@@ -116,21 +116,21 @@ double Ctrl_ADJ_plantar(Steps* p_steps, int N3, double* p_FSRatio, double* p_Max
   if (flag_torque_time_volt != 0){
     double new_setpoint;
     if (flag_torque_time_volt == 2) {
-      new_setpoint = (p_steps->desired_setpoint ) * (prop_gain) * (*p_FSRatio);
+      new_setpoint = (motor_steps->desired_setpoint ) * (prop_gain) * (*p_FSRatio);
 
     } else if (flag_torque_time_volt == 3) {
       // Second version of pivot proportional Ctrl with polynomial law XXXXX QUI METTERE LEGGE
-      new_setpoint =(p_steps->desired_setpoint ) *
+      new_setpoint =(motor_steps->desired_setpoint ) *
         (p_prop[0] * pow(*p_FSRatio, 2) + p_prop[1] * (*p_FSRatio) + p_prop[2]) / (p_prop[0] + p_prop[1] + p_prop[2]) ;
 
     } else if (flag_torque_time_volt == 1) {
 
-      new_setpoint = (p_steps->desired_setpoint ) *
+      new_setpoint = (motor_steps->desired_setpoint ) *
         (p_prop[0] * pow((*p_Max_FSRatio), 2) + p_prop[1] * (*p_Max_FSRatio) + p_prop[2]) / (p_prop[0] + p_prop[1] + p_prop[2]);
 
       N3 = 1;
     }
-    new_setpoint = clamp_setpoint(new_setpoint, p_steps->setpoint_clamp);
+    new_setpoint = clamp_setpoint(new_setpoint, motor_steps->setpoint_clamp);
     *p_Setpoint_Ankle_Pctrl = new_setpoint;
     return N3;
   }
@@ -138,31 +138,31 @@ double Ctrl_ADJ_plantar(Steps* p_steps, int N3, double* p_FSRatio, double* p_Max
   // Otherwise we need to calculate the time
 
   // Parameters for speed adaption
-  if (p_steps->flag_start_plant == false) // if it is true it means you started the step. Here I inizialize the parameters for speed adaption.
+  if (leg_steps->flag_start_plant == false) // if it is true it means you started the step. Here I inizialize the parameters for speed adaption.
   {
-    p_steps->plant_time = millis(); // start the plantarflexion
-    p_steps->dorsi_time = millis() - (p_steps->dorsi_time); // calculate the dorsiflexion that has just finished
+    leg_steps->plant_time = millis(); // start the plantarflexion
+    leg_steps->dorsi_time = millis() - (leg_steps->dorsi_time); // calculate the dorsiflexion that has just finished
 
-    if (p_steps->dorsi_time <= step_time_length / 4) // if <50ms probably it is noise
+    if (leg_steps->dorsi_time <= step_time_length / 4) // if <50ms probably it is noise
     {
-      p_steps->peak = 0;
-      p_steps->flag_start_plant = false;
+      FSR_steps->peak_voltage = 0;
+      leg_steps->flag_start_plant = false;
       return N3;
     }
 
-    p_steps->flag_start_plant = true; // Parameters inizialized Start a step
+    leg_steps->flag_start_plant = true; // Parameters inizialized Start a step
   }
 
 
   // Torque adaption as a function of the speed or of the pressure force
   // If you want to adjust the torque and hence torque_adj = 1
   //-----------------add 11:49 6/19/18
-  if (p_steps->torque_adj)
+  if (motor_steps->torque_adj)
   {
-    if (p_steps->plant_time <= step_time_length)
+    if (leg_steps->plant_time <= step_time_length)
     {
-      p_steps->peak = 0;
-      p_steps->flag_start_plant = false;
+      FSR_steps->peak_voltage = 0;
+      leg_steps->flag_start_plant = false;
       Serial.println(" TRQ ADJ plant time too short ");
       return N3;
     }
@@ -170,14 +170,14 @@ double Ctrl_ADJ_plantar(Steps* p_steps, int N3, double* p_FSRatio, double* p_Max
 
     // if you use plantar time as reference to increase the torque
     double new_setpoint;
-    if (p_steps->flag_start_plant == true) {
+    if (leg_steps->flag_start_plant == true) {
       if (flag_torque_time_volt == 0) {
         // if you're going use the time as reference to increase also the torque
-        new_setpoint = (p_steps->desired_setpoint ) * (1 / (fabs(p_steps->plant_mean)));
+        new_setpoint = (motor_steps->desired_setpoint ) * (1 / (fabs(leg_steps->plant_mean)));
       } else if (flag_torque_time_volt == 1) { // If you use the volt or force value returned by the FSR sensors
-        new_setpoint = (p_steps->desired_setpoint ) * (fabs(p_steps->peak / p_steps->plant_peak_mean));
+        new_setpoint = (motor_steps->desired_setpoint ) * (fabs(FSR_steps->peak_voltage / FSR_steps->plant_peak_mean));
       }
-      new_setpoint = clamp_setpoint(new_setpoint, p_steps->setpoint_clamp);
+      new_setpoint = clamp_setpoint(new_setpoint, motor_steps->setpoint_clamp);
       *p_Setpoint_Ankle = new_setpoint;
     }
   }// end torque adj
@@ -187,40 +187,40 @@ double Ctrl_ADJ_plantar(Steps* p_steps, int N3, double* p_FSRatio, double* p_Max
 
 double Ctrl_ADJ_dorsi(Steps* p_steps, double N3){
 
-  if (p_steps->flag_start_plant) { // If a step has started i.e. the states have passed from 1 or 2 to 3
+  if (leg_steps->flag_start_plant) { // If a step has started i.e. the states have passed from 1 or 2 to 3
     // if you transit from 3 to 1 plantar flexion is completed and start dorsiflexion
 
-    p_steps->count_plant++; // you have accomplished a step
+    leg_steps->count_plant++; // you have accomplished a step
 
     // start dorsiflexion
-    p_steps->dorsi_time = millis();
+    leg_steps->dorsi_time = millis();
 
     // calculate plantarflexion
-    p_steps->plant_time = millis() - (p_steps->plant_time);
+    leg_steps->plant_time = millis() - (leg_steps->plant_time);
 
-    if (p_steps->plant_time <= step_time_length) {
-      p_steps->flag_start_plant = false;
+    if (leg_steps->plant_time <= step_time_length) {
+      leg_steps->flag_start_plant = false;
       return N3;
     }
 
-    p_steps->flag_start_plant = false; // you have provided one step
+    leg_steps->flag_start_plant = false; // you have provided one step
 
-    if (p_steps->count_plant >= 2) {
+    if (leg_steps->count_plant >= 2) {
       // this is the time window of the filter for the plantarflexion
-      p_steps->plant_mean =
-        take_baseline_get_mean_time(p_steps->plant_time, p_steps->plant_mean,
-                                    p_steps, p_steps->plant_time_averager);
+      leg_steps->plant_mean =
+        take_baseline_get_mean(leg_steps->plant_time, leg_steps->plant_mean,
+                               p_steps, leg_steps->plant_time_averager);
 
       // Update mean value
-      p_steps->plant_mean = p_steps->plant_time_averager->getAverage();
+      leg_steps->plant_mean = leg_steps->plant_time_averager->getAverage();
     }
 
-    if (p_steps->flag_N3_adjustment_time) {
-      N3 = round((p_steps->plant_mean) * p_steps->perc);
+    if (motor_steps->flag_N3_adjustment_time) {
+      N3 = round((leg_steps->plant_mean) * motor_steps->perc);
       N3 = min(500, max(4, N3));
     }
   }// end if flag_start_plant
-  p_steps->peak = 0;
+  FSR_steps->peak_voltage = 0;
   p_Max_FSRatio = 0;
   return N3;
 }
@@ -229,7 +229,7 @@ double Ctrl_ADJ(int state, int state_old, Steps* p_steps, double N3, double New_
                 double* p_Setpoint_Ankle, double * p_Setpoint_Ankle_Pctrl, int flag_torque_time_volt,
                 double prop_gain, double taking_baseline, double *p_FSRatio, double* p_Max_FSRatio) {
 
-  // Speed adjustment -> the smoothing parameters are updated as a function of the plantar time in order to modify the
+// Speed adjustment -> the smoothing parameters are updated as a function of the plantar time in order to modify the
   // shaping of the torque to the new step time.
 
   // It considers the average time of 4 steps. There's a filter of step_time_length on the plantarflexion time in order
@@ -248,10 +248,10 @@ double Ctrl_ADJ(int state, int state_old, Steps* p_steps, double N3, double New_
     // if you transit from state 1 to state 3 dorsiflexion is completed and start plantarflexion
   if ((state == LATE_STANCE) && (state_old == SWING)) {
     // update the voltage peak to update torque in case of Bang Bang ctrl
-    if (p_steps->curr_voltage > p_steps->peak)
-      p_steps->peak =  p_steps->curr_voltage;
+    if (FSR_steps->curr_voltage > FSR_steps->peak_voltage)
+      FSR_steps->peak_voltage =  FSR_steps->curr_voltage;
 
-    *p_FSRatio = fabs(p_steps->curr_voltage / p_steps->plant_peak_mean);
+    *p_FSRatio = fabs(FSR_steps->curr_voltage / FSR_steps->plant_peak_mean);
     if (*p_FSRatio > (*p_Max_FSRatio))
       (*p_Max_FSRatio) = *p_FSRatio;
     N3 = Ctrl_ADJ_plantar(p_steps, N3, p_FSRatio, p_Max_FSRatio,
