9acd48c074b48b0a824f132da94fd44239f98d35
Jack Garrard <jg2562@nau.edu>
2018-11-06 19:09:47 -0700

    Fixed the control algorithms header

--- Control_Algorithms.h
+++ Control_Algorithms.h
@@ -1,5 +1,8 @@
-#include "Control_Algorithms.h"
+#ifndef CONTROL_ALGORITHMS_HEADER
+#define CONTROL_ALGORITHMS_HEADER
 #include "Utils.h"
 
 double applyControlAlgorithm(int control_algorithm, double desired_setpoint,
                              Clamp* setpoint_clamp, double FSRatio, double Max_FSRatio);
+
+#endif
