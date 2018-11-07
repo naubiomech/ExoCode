3974a4492ed4af4f05eed9b5a8deb3cd6f44774f
Jack Garrard <jg2562@nau.edu>
2018-11-06 19:27:34 -0700

    Updated the average class name to running average

--- Auto_KF.h
+++ Auto_KF.h
@@ -2,7 +2,7 @@
 #define AUTO_KF_HEADER
 #include "Utils.h"
 
-void Auto_KF_motor_Late_stance(Average* error_average, double pid_setpoint, double motor_input);
-double Auto_KF_motor_Swing(Average* error_average, double KF, Clamp* kf_clamp);
+void Auto_KF_motor_Late_stance(RunningAverage* error_average, double pid_setpoint, double motor_input);
+double Auto_KF_motor_Swing(RunningAverage* error_average, double KF, Clamp* kf_clamp);
 
 #endif
--- Auto_KF.ino
+++ Auto_KF.ino
@@ -2,11 +2,11 @@
 #include "Parameters.h"
 #include "Utils.h"
 
-void Auto_KF_motor_Late_stance(Average* error_average, double pid_setpoint, double motor_input){
+void Auto_KF_motor_Late_stance(RunningAverage* error_average, double pid_setpoint, double motor_input){
   error_average->update(pid_setpoint - motor_input);
 }
 
-double Auto_KF_motor_Swing(Average* error_average, double KF, Clamp* kf_clamp){
+double Auto_KF_motor_Swing(RunningAverage* error_average, double KF, Clamp* kf_clamp){
   double err = error_average->getAverage();
   error_average->reset();
 
