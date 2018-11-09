cd96ba764e57e85ee8ec1984344bc5b7d91750b2
Jack Garrard <jg2562@nau.edu>
2018-11-08 17:28:46 -0700

    Added the control algorithm enum and adapted control algorithm to it

--- Control_Algorithms.h
+++ Control_Algorithms.h
@@ -2,7 +2,9 @@
 #define CONTROL_ALGORITHMS_HEADER
 #include "Utils.h"
 
-double applyControlAlgorithm(int control_algorithm, double desired_setpoint,
-                             Clamp* setpoint_clamp, double FSRatio, double Max_FSRatio);
+enum ControlAlgorithm {zero_torque,bang_bang, unknown_control, porportional,pivot_porportional};
+
+double getControlAlgorithmSetpoint(ControlAlgorithm control_algorithm, double desired_setpoint,
+                                   Clamp* setpoint_clamp, double FSRatio, double Max_FSRatio);
 
 #endif
--- Control_Algorithms.ino
+++ Control_Algorithms.ino
@@ -10,21 +10,23 @@ double clamp_setpoint(double raw_setpoint, Clamp* setpoint_clamp){
   return setpoint;
 }
 
-double applyControlAlgorithm(int control_algorithm, double desired_setpoint,
-                             Clamp* setpoint_clamp, double FSRatio, double Max_FSRatio){
+double getControlAlgorithmSetpoint(ControlAlgorithm control_algorithm, double desired_setpoint,
+                                   Clamp* setpoint_clamp, double FSRatio, double Max_FSRatio){
   double new_setpoint;
 
-  if (control_algorithm == 2) {
-    new_setpoint = (desired_setpoint ) * (prop_gain) * (FSRatio);
-  } else if (control_algorithm == 3) {
-    // Second version of pivot proportional Ctrl with polynomial law XXXXX QUI METTERE LEGGE
-    new_setpoint =(desired_setpoint ) *
-      (p_prop[0] * pow(FSRatio, 2) + p_prop[1] * (FSRatio) + p_prop[2]) / (p_prop[0] + p_prop[1] + p_prop[2]) ;
-  } else if (control_algorithm == 1) {
+  switch (control_algorithm){
+  case unknown_control:
     new_setpoint = (desired_setpoint ) *
       (p_prop[0] * pow((Max_FSRatio), 2) + p_prop[1] * (Max_FSRatio) + p_prop[2]) / (p_prop[0] + p_prop[1] + p_prop[2]);
+    break;
+  case porportional:
+    new_setpoint = (desired_setpoint ) * (prop_gain) * (FSRatio);
+    break;
+  case pivot_porportional:
+    new_setpoint =(desired_setpoint ) *
+      (p_prop[0] * pow(FSRatio, 2) + p_prop[1] * (FSRatio) + p_prop[2]) / (p_prop[0] + p_prop[1] + p_prop[2]) ;
+    break;
   }
-
   new_setpoint = clamp_setpoint(new_setpoint, setpoint_clamp);
 
   return new_setpoint;
