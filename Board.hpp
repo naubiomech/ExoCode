779f7afe6f2e7a68b447f43d161b58e5098cdb55
Jack Garrard <jg2562@nau.edu>
2018-11-11 20:44:54 -0700

    Cleaned up a few pins and changed how imu enable and pwm control are defined

--- Board.hpp
+++ Board.hpp
@@ -10,8 +10,6 @@ ExoSystem* setupBoard();
 const i2c_pins IMU_SLOT_1_PINS = I2C_PINS_3_4;
 const i2c_pins IMU_SLOT_2_PINS = I2C_PINS_7_8;
 const i2c_pins IMU_SLOT_3_PINS = I2C_PINS_37_38;
-const bool IMU_ENABLED = false;
-const bool PWM_CONTROL = false;
 
 const unsigned int BLUETOOTH_TX_PIN = 0;
 const unsigned int BLUETOOTH_RX_PIN = 1;
@@ -31,21 +29,12 @@ const unsigned int LED_PIN = 13;
 
 const unsigned int MOTOR_ENABLE_PIN = A22;
 
-const unsigned int WHICH_LEG_PIN = 15;
-
 const unsigned int MOTOR_ERROR_LEFT_ANKLE_PIN = A5;
 const unsigned int MOTOR_ERROR_RIGHT_ANKLE_PIN = A7;
-
-const i2c_pins IMU_1_PINS = IMU_SLOT_2_PINS;
-
-const unsigned int MOTOR_COUNT = 2;
 
 #endif
 
 #ifdef TWO_LEG_BOARD
-const bool IMU_ENABLED = false;
-const bool PWM_CONTROL = false;
-
 const unsigned int BLUETOOTH_TX_PIN = 0;
 const unsigned int BLUETOOTH_RX_PIN = 1;
 
@@ -64,21 +53,16 @@ const unsigned int LED_PIN = 13;
 
 const unsigned int MOTOR_ENABLE_PIN = 22;
 
-const unsigned int WHICH_LEG_PIN = 15;
-
 const unsigned int MOTOR_ERROR_LEFT_ANKLE_PIN = 6;
 const unsigned int MOTOR_ERROR_RIGHT_ANKLE_PIN = 7;
-
-const unsigned int MOTOR_COUNT = 2;
 #endif
 
 #ifdef QUAD_BOARD
 #include <i2c_t3.h>
+#define PWM_CONTROL
 const i2c_pins IMU_SLOT_0_PINS = I2C_PINS_7_8;
 const i2c_pins IMU_SLOT_1_PINS = I2C_PINS_37_38;
 const i2c_pins IMU_SLOT_2_PINS = I2C_PINS_3_4;
-const bool IMU_ENABLED = false;
-const bool PWM_CONTROL = true;
 
 const unsigned int BLUETOOTH_TX_PIN = 0;
 const unsigned int BLUETOOTH_RX_PIN = 1;
@@ -102,15 +86,22 @@ const unsigned int LED_PIN = 13;
 
 const unsigned int MOTOR_ENABLE_PIN = 17;
 
-const unsigned int WHICH_LEG_PIN = 15;
-
 const unsigned int MOTOR_ERROR_LEFT_KNEE_PIN = 24;
 const unsigned int MOTOR_ERROR_LEFT_ANKLE_PIN = 25;
 const unsigned int MOTOR_ERROR_RIGHT_KNEE_PIN = 26;
 const unsigned int MOTOR_ERROR_RIGHT_ANKLE_PIN = 27;
-
-const i2c_pins IMU_1_PINS = IMU_SLOT_2_PINS;
-
-const unsigned int MOTOR_COUNT = 4;
 #endif
+
+#ifdef PWM_CONTROL
+const bool PWM_CONTROL = true;
+#else
+const bool PWM_CONTROL = false;
+#endif
+
+#ifdef IMU_ENABLED
+const bool IMU_ENABLED = true;
+#else
+const bool IMU_ENABLED = false;
+#endif
+
 #endif
