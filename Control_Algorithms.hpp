7c3f85b917b50fa99e14773cc26cf8c78a80fc50
Jack Garrard <jg2562@nau.edu>
2018-11-15 18:09:34 -0700

    Added a space to the control algorithm

--- Control_Algorithms.hpp
+++ Control_Algorithms.hpp
@@ -2,7 +2,7 @@
 #define CONTROL_ALGORITHMS_HEADER
 #include "Utils.hpp"
 
-enum ControlAlgorithm {zero_torque,bang_bang, unknown_control, porportional,pivot_porportional};
+enum ControlAlgorithm {zero_torque, bang_bang, unknown_control, porportional,pivot_porportional};
 
 double getSetpoint(ControlAlgorithm control_algorithm, double desired_setpoint,
                    Clamp* setpoint_clamp, double FSRatio, double Max_FSRatio, double prop_gain);
