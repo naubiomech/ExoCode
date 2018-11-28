37bbafb80413811e54045b576562ae4d1f501823
Jack Garrard <jg2562@nau.edu>
2018-11-27 18:05:43 -0700

    Moved the leg builder to the hpp file

--- ExoBuilder.cpp
+++ ExoBuilder.cpp
@@ -8,30 +8,6 @@
 #include "States.hpp"
 #include "FSR.hpp"
 
-class LegBuilder{
-private:
-  ExoBuilder* return_context;
-  int sign;
-  State* states;
-  std::vector<InputPort*> fsr_ports_begin;
-  std::vector<InputPort*> torque_sensor_ports;
-  std::vector<OutputPort*> motor_ports;
-  std::vector<InputPort*> error_ports;
-  std::vector<std::vector<InputPort*>> fsr_ports;
-  std::vector<ImuPort*> imu_ports;
-  std::vector<unsigned int> imu_address;
-  std::vector<ControlModule*> controls;
-public:
-  LegBuilder(ExoBuilder* return_context);
-  LegBuilder* addStateMachine(State* states);
-  LegBuilder* addJoint(InputPort* torque_sensor_port, OutputPort* motor_port,
-                       InputPort* error_port, ControlModule* module);
-  LegBuilder* beginFSRGroup();
-  LegBuilder* finishFSRGroup();
-  LegBuilder* addFSR(InputPort* fsr_port);
-  ExoBuilder* finishLeg();
-  Leg* build();
-  };
 
 
   ExoBuilder* ExoBuilder::addTransceiver(TxPort* tx, RxPort* rx){
--- ExoBuilder.hpp
+++ ExoBuilder.hpp
@@ -18,4 +18,30 @@ public:
   Exoskeleton* build();
 };
 
+class LegBuilder{
+private:
+  ExoBuilder* return_context;
+  int sign;
+  State* states;
+  std::vector<InputPort*> fsr_ports_begin;
+  std::vector<InputPort*> torque_sensor_ports;
+  std::vector<OutputPort*> motor_ports;
+  std::vector<InputPort*> error_ports;
+  std::vector<std::vector<InputPort*>> fsr_ports;
+  std::vector<ImuPort*> imu_ports;
+  std::vector<unsigned int> imu_address;
+  std::vector<ControlModule*> controls;
+public:
+  LegBuilder(ExoBuilder* return_context);
+  LegBuilder* addStateMachine(State* states);
+  LegBuilder* addJoint(InputPort* torque_sensor_port, OutputPort* motor_port,
+                       InputPort* error_port, ControlModule* module);
+  LegBuilder* beginFSRGroup();
+  LegBuilder* finishFSRGroup();
+  LegBuilder* addFSR(InputPort* fsr_port);
+  ExoBuilder* finishLeg();
+  Leg* build();
+};
+
+
 #endif
