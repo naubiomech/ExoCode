c892c309e9d28789a0c30ba36db9720c37cd1f90
Jack Garrard <jg2562@nau.edu>
2018-12-03 10:50:12 -0700

    Changed bearings to be initialized in initializer

--- IMU.cpp
+++ IMU.cpp
@@ -6,6 +6,10 @@ IMU::IMU(ImuPort* imu_port, unsigned int address){
   bno = new Adafruit_BNO055(WIRE_BUS, 1, address, I2C_MASTER, imu_port->getPins(),
                             I2C_PULLUP_EXT, I2C_RATE_100, I2C_OP_MODE_ISR);
 
+  for(int i = 0; i < 3;i++){
+    bearings[i] = 0;
+  }
+
   if (!bno->begin()) {
     Serial.println("No IMU detected");
   }
@@ -51,8 +55,8 @@ void IMU::calibrate(){
 
 void IMU::measure(){
   if (imu_measure_limiter.check()){
-    sensors_event_t event;
-    bno->getEvent(&event);
+	  sensors_event_t event;
+	  bno->getEvent(&event);
     this->bearings[0] = event.orientation.x;
     this->bearings[1] = event.orientation.y;
     this->bearings[2] = event.orientation.z;
