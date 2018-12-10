3ef0eaee880d21a4b9ea51c0e5075365ae42692a
Jack Garrard <jg2562@nau.edu>
2018-12-09 18:34:05 -0700

    Created a message class

--- Message.cpp
+++ Message.cpp
@@ -1,2 +1,19 @@
 #include "Message.hpp"
+#include "Leg.hpp"
+#include "Joint.hpp"
 
+void LegMessage::messageJoints(LinkedList<Joint*>* joints){
+  ListIterator<Joint*> joint_iter = joints->getIterator();
+  ListIterator<JointMessage*> msg_iter = joint_msgs->getIterator();
+  while(joint_iter.hasNext() && msg_iter.hasNext()){
+    joint_iter.next()->processMessage(msg_iter.next());
+  }
+}
+
+void ExoMessage::messageLeftLeg(Leg* left){
+  left->processMessage(left_leg_msg);
+}
+
+void ExoMessage::messageRightLeg(Leg* right){
+  right->processMessage(right_leg_msg);
+}
--- Message.hpp
+++ Message.hpp
@@ -2,55 +2,74 @@
 #define EXO_MESSAGE_HEADER
 #include <cstddef>
 #include "Report.hpp"
+#include "Linked_List.hpp"
+#include "Commands.hpp"
 
-enum LegMessageIndex{RIGHT_LEG_MSG, LEFT_LEG_MSG};
+class Exoskeleton;
+class Leg;
+class Joint;
 
-class Command{
+template <class Context>
+class Message{
+private:
+  LinkedList<Command<Context>*>* pre_commands;
+  LinkedList<Command<Context>*>* post_commands;
+  void runCommands(Context* context, LinkedList<Command<Context>*>* cmds);
 public:
-  virtual void execute();
+  void runPreCommands(Context* context);
+  void runCommands(Context* context);
+  void runPostCommands(Context* context);
 };
 
-class JointCommand:Command{
+class JointMessage:public Message<Joint>{
 public:
-  virtual void execute();
+  JointMessage(LinkedList<ExoCommand*>* commands);
 };
 
-class LegCommand:Command{
+class LegMessage:public Message<Leg>{
+private:
+  LinkedList<JointMessage*>* joint_msgs;
 public:
-  virtual void execute();
+  LegMessage(LinkedList<LegCommand*>* pre_commands, LinkedList<LegCommand*>* post_commands);
+  void messageJoints(LinkedList<Joint*>* joints);
 };
 
-class ExoCommand:Command{
-public:
-  virtual void execute();
-};
-
-class StartTrialCommand:ExoCommand{
-public:
-  virtual void execute();
-};
-
-class JointMessage{
-public:
-  JointCommand* command;
-};
-
-class LegMessage{
-public:
-  LegCommand* command;
-  JointMessage* joint_msg;
-  int joint_index;
-};
-
-class ExoMessage{
-public:
+class ExoMessage:public Message<Exoskeleton>{
+private:
   LegMessage* right_leg_msg;
   LegMessage* left_leg_msg;
-  ExoCommand* command;
+public:
+  ExoMessage(LinkedList<ExoCommand*>* pre_commands, LinkedList<ExoCommand*>* post_commands);
+  void messageRightLeg(Leg* right);
+  void messageLeftLeg(Leg* left);
 };
 
-class MessageFactory{
-  ExoMessage* createJointMessage(JointCommand* command, LegMessageIndex leg_index,int joint_index);
-  ExoMessage* createLegMessage(JointCommand* command);
-};
+template<class Context>
+void Message<Context>::runCommands(Context* context, LinkedList<Command<Context>*>* cmds){
+  if (cmds == NULL){
+    return;
+  }
+
+  ListIterator<Command<Context>*> iter = cmds->getIterator();
+  while(iter.hasNext()){
+    Command<Context>* cmd = iter.next();
+    cmd->execute(context);
+  }
+}
+
+template<class Context>
+void Message<Context>::runPreCommands(Context* context){
+  runCommands(context, pre_commands);
+}
+
+template<class Context>
+void Message<Context>::runCommands(Context* context){
+  runPreCommands(context);
+}
+
+template<class Context>
+void Message<Context>::runPostCommands(Context* context){
+  runCommands(context, post_commands);
+}
+
 #endif
