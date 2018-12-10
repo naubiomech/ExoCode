ac953bb0affcfcd43092fcd9978016127a9a5c23
Jack Garrard <jg2562@nau.edu>
2018-12-09 22:35:23 -0700

    Created a message builder system

--- Message.cpp
+++ Message.cpp
@@ -2,6 +2,13 @@
 #include "Leg.hpp"
 #include "Joint.hpp"
 
+LegMessage::LegMessage(LinkedList<Command<Leg>*>* pre_commands,
+                       LinkedList<Command<Leg>*>* post_commands,
+                       LinkedList<JointMessage*>* joint_msgs):Message<Leg>(pre_commands, post_commands){
+                         this->joint_msgs = joint_msgs;
+};
+
+
 void LegMessage::messageJoints(LinkedList<Joint*>* joints){
   ListIterator<Joint*> joint_iter = joints->getIterator();
   ListIterator<JointMessage*> msg_iter = joint_msgs->getIterator();
@@ -20,8 +27,76 @@ void ExoMessage::messageRightLeg(Leg* right){
 
 JointMessage::JointMessage(LinkedList<Command<Joint>*>* commands):Message<Joint>(commands){};
 
-LegMessage::LegMessage(LinkedList<Command<Leg>*>* pre_commands,
-                       LinkedList<Command<Leg>*>* post_commands):Message<Leg>(pre_commands, post_commands){};
-
 ExoMessage::ExoMessage(LinkedList<Command<Exoskeleton>*>* pre_commands,
-                       LinkedList<Command<Exoskeleton>*>* post_commands):Message<Exoskeleton>(pre_commands, post_commands){};
+                       LinkedList<Command<Exoskeleton>*>* post_commands,
+                       LegMessage* right_leg_msg,
+                       LegMessage* left_leg_msg):Message<Exoskeleton>(pre_commands, post_commands){
+                         this->right_leg_msg = right_leg_msg;
+                         this->left_leg_msg = left_leg_msg;
+};
+
+JointMessageBuilder::JointMessageBuilder(LegMessageBuilder* return_context){
+  this->return_context = return_context;
+}
+
+LegMessageBuilder* JointMessageBuilder::finishJoint(){
+  return return_context;
+}
+
+JointMessage* JointMessageBuilder::build(){
+  JointMessage* joint_msg = new JointMessage(getCommands());
+  clearCommands();
+  return joint_msg;
+}
+
+LegMessageBuilder::LegMessageBuilder(ExoMessageBuilder* return_context){
+  this->return_context = return_context;
+}
+
+JointMessageBuilder* LegMessageBuilder::beginJointMessage(unsigned int id){
+  while ((id+1) < joint_builders.size()){
+    joint_builders.append(new JointMessageBuilder(this));
+  }
+  return joint_builders[id];
+}
+
+ExoMessageBuilder* LegMessageBuilder::finishLeg(){
+  return return_context;
+}
+
+LegMessage* LegMessageBuilder::build(){
+  LinkedList<JointMessage*>* joint_msgs = new LinkedList<JointMessage*>();
+  ListIterator<JointMessageBuilder*> joint_builder_iter = joint_builders.getIterator();
+  while(joint_builder_iter.hasNext()){
+    joint_msgs->append(joint_builder_iter.next()->build());
+  }
+  LegMessage* leg_msg = new LegMessage(getPreCommands(), getPostCommands(), joint_msgs);
+  clearCommands();
+  return leg_msg;
+}
+
+ExoMessageBuilder::ExoMessageBuilder(){
+  left_builder = NULL;
+  right_builder = NULL;
+}
+
+LegMessageBuilder* ExoMessageBuilder::beginLeftLegMessage(){
+  if(left_builder == NULL){
+    left_builder = new LegMessageBuilder(this);
+  }
+  return left_builder;
+}
+
+LegMessageBuilder* ExoMessageBuilder::beginRightLegMessage(){
+  if(right_builder == NULL){
+    right_builder = new LegMessageBuilder(this);
+  }
+  return right_builder;
+}
+
+ExoMessage* ExoMessageBuilder::build(){
+  ExoMessage* exo_msg = new ExoMessage(getPreCommands(), getPostCommands(),
+                                       right_builder->build(), left_builder->build());
+  this->clearCommands();
+  return exo_msg;
+}
--- Message.hpp
+++ Message.hpp
@@ -32,7 +32,8 @@ class LegMessage:public Message<Leg>{
 private:
   LinkedList<JointMessage*>* joint_msgs;
 public:
-  LegMessage(LinkedList<Command<Leg>*>* pre_commands, LinkedList<Command<Leg>*>* post_commands);
+  LegMessage(LinkedList<Command<Leg>*>* pre_commands, LinkedList<Command<Leg>*>* post_commands,
+             LinkedList<JointMessage*>* joint_msgs);
   void messageJoints(LinkedList<Joint*>* joints);
 };
 
@@ -41,9 +42,70 @@ private:
   LegMessage* right_leg_msg;
   LegMessage* left_leg_msg;
 public:
-  ExoMessage(LinkedList<Command<Exoskeleton>*>* pre_commands, LinkedList<Command<Exoskeleton>*>* post_commands);
+  ExoMessage(LinkedList<Command<Exoskeleton>*>* pre_commands, LinkedList<Command<Exoskeleton>*>* post_commands,
+             LegMessage* right_leg_msg, LegMessage* left_leg_msg);
   void messageRightLeg(Leg* right);
   void messageLeftLeg(Leg* left);
+};
+
+
+
+template <class T, class Context>
+class MessageBuilder{
+private:
+  LinkedList<Command<T>*>  pre_commands;
+  LinkedList<Command<T>*>  post_commands;
+protected:
+  LinkedList<Command<T>*>* getPreCommands();
+  LinkedList<Command<T>*>* getPostCommands();
+  void clearCommands();
+public:
+  Context* addPreCommand(Command<T>* command);
+  Context* addPostCommand(Command<T>* command);
+};
+
+template <class T, class Context>
+class SingleMessageBuilder:private MessageBuilder<T, Context>{
+protected:
+  LinkedList<Command<T>*>* getCommands();
+  void clearCommands();
+public:
+  Context* addCommand(Command<T>* command);
+};
+class JointMessageBuilder;
+class LegMessageBuilder;
+class ExoMessageBuilder;
+
+
+class JointMessageBuilder:public SingleMessageBuilder<Joint, JointMessageBuilder>{
+private:
+  LegMessageBuilder* return_context;
+public:
+  JointMessageBuilder(LegMessageBuilder* return_context);
+  LegMessageBuilder* finishJoint();
+  JointMessage* build();
+};
+
+class LegMessageBuilder:public MessageBuilder<Leg, LegMessageBuilder>{
+private:
+  ExoMessageBuilder* return_context;
+  LinkedList<JointMessageBuilder*> joint_builders;
+public:
+  LegMessageBuilder(ExoMessageBuilder* return_context);
+  JointMessageBuilder* beginJointMessage(unsigned int id);
+  ExoMessageBuilder* finishLeg();
+  LegMessage* build();
+};
+
+class ExoMessageBuilder:public MessageBuilder<Exoskeleton, ExoMessageBuilder>{
+private:
+  LegMessageBuilder* left_builder;
+  LegMessageBuilder* right_builder;
+public:
+  ExoMessageBuilder();
+  LegMessageBuilder* beginLeftLegMessage();
+  LegMessageBuilder* beginRightLegMessage();
+  ExoMessage* build();
 };
 
 template<class Context>
@@ -86,4 +148,48 @@ void Message<Context>::runPostCommands(Context* context){
   runCommands(context, post_commands);
 }
 
+template<class T,class Context>
+Context* MessageBuilder<T, Context>::addPreCommand(Command<T>* command){
+  pre_commands.append(command);
+  return this;
+}
+
+template<class T, class Context>
+Context* MessageBuilder<T, Context>::addPostCommand(Command<T>* command){
+  post_commands.append(command);
+  return this;
+}
+
+template<class T, class Context>
+LinkedList<Command<T>*>* MessageBuilder<T, Context>::getPreCommands(){
+  return &pre_commands;
+}
+
+template<class T, class Context>
+LinkedList<Command<T>*>* MessageBuilder<T, Context>::getPostCommands(){
+  return &post_commands;
+}
+
+template<class T, class Context>
+void MessageBuilder<T, Context>::clearCommands(){
+  pre_commands.clear();
+  post_commands.clear();
+}
+
+template<class T, class Context>
+Context* SingleMessageBuilder<T, Context>::addCommand(Command<T>* command){
+  this->addPreCommand(command);
+  return this;
+}
+
+template<class T, class Context>
+LinkedList<Command<T>*>* SingleMessageBuilder<T, Context>::getCommands(){
+  return this->getPreCommands();
+}
+
+
+template<class T, class Context>
+void SingleMessageBuilder<T, Context>::clearCommands(){
+  MessageBuilder<T, Context>::clearCommands();
+}
 #endif
