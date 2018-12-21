0ea9087ef9a7f7bd3967975dc01010937e945dd4
Jack Garrard <jg2562@nau.edu>
2018-12-20 23:56:42 -0700

    Added the torque conversion

--- Parameters.hpp
+++ Parameters.hpp
@@ -11,7 +11,6 @@ const bool FLAG_PID_VALS = false;
 const bool FLAG_TWO_TOE_SENSORS = false;
 
 const double PID_DEFAULTS[] = {4, 0.1, 0};
-
 const int TORQUE_CALIBRATION_TIME_IN_MS = 1000;
 
 const double MOTOR_ZERO_OFFSET_DEFAULT = 0.0;
@@ -69,5 +68,6 @@ const double DEFAULT_ITER_LATE_STANCE = 4;
 const int FSR_CALIBRATION_PEAK_COUNT = 4;
 const double FSR_UPPER_THRESHOLD = 0.7;
 const double FSR_LOWER_THRESHOLD = 0.3;
+const double VOLTAGE_TO_TORQUE_CONVERSION = 3.3 * 56.5 / 2.1;
 
 #endif
