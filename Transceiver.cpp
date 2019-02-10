d441a04c759dc6c658924726b33da544f691c0c2
Jack Garrard <jg2562@nau.edu>
2019-02-09 20:14:35 -0700

    Formatted transceiver

--- Transceiver.cpp
+++ Transceiver.cpp
@@ -9,77 +9,77 @@ Transceiver::~Transceiver(){
 }
 
 void MatlabTransceiver::clear(){
-	while(dataAvailable()){
-		serial->read();
-	}
+  while(dataAvailable()){
+    serial->read();
+  }
 }
 
 bool Transceiver::noDataAvailable(){
-	return !dataAvailable();
+  return !dataAvailable();
 }
 
 MatlabTransceiver::MatlabTransceiver(TxPort* tx, RxPort* rx){
-	serial = new SoftwareSerial(tx->getPin(), rx->getPin());
-	serial->begin(115200);
-	delete tx;
-	delete rx;
+  serial = new SoftwareSerial(tx->getPin(), rx->getPin());
+  serial->begin(115200);
+  delete tx;
+  delete rx;
 }
 
 MatlabTransceiver::~MatlabTransceiver(){
-	// Commented command serial delete until software serial allows deletion
-	/* delete command_serial; */
+  // Commented command serial delete until software serial allows deletion
+  /* delete command_serial; */
 }
 
 bool MatlabTransceiver::dataAvailable(){
-	return serial->available() > 0;
+  return serial->available() > 0;
 }
 
 bool MatlabTransceiver::receiveHeader(){
-	return true;
+  return true;
 }
 
 CommandCode MatlabTransceiver::receiveCommand(){
-	return serial->read();
+  return serial->read();
 }
 
 bool MatlabTransceiver::receiveFooter(){
-	return true;
+  return true;
 }
 
 void MatlabTransceiver::receiveData(double* output_data, int doubles_expected){
-	if (doubles_expected <= 0){
-		return;
-	}
-
-	char* output_data_raw_form = (char*) output_data;
-	for(int i = 0; i < doubles_expected; i ++){
-		while (noDataAvailable()){}
-		int data = serial->read();
-		output_data_raw_form[i] = data;
-	}
+  if (doubles_expected <= 0){
+    return;
+  }
+  doubles_expected *= sizeof(double);
+  char* output_data_raw_form = (char*) output_data;
+  for(int i = 0; i < doubles_expected; i ++){
+    while (noDataAvailable()){}
+    int data = serial->read();
+    output_data_raw_form[i] = data;
+  }
 }
 
 void MatlabTransceiver::sendHeader(){
-	serial->write('S');
+  serial->write('S');
 }
 
 void MatlabTransceiver::sendCommand(CommandCode code){
-	serial->write(code);
-	serial->write(',');
+  serial->write(code);
+  serial->write(',');
 }
 
 void MatlabTransceiver::sendData(double* data, int doubles_to_send){
-	if (doubles_to_send <= 0){
-		return;
-	}
+  if (doubles_to_send <= 0){
+    return;
+  }
 
-	for (int i = 0; i < doubles_to_send; i++){
-		serial->print(data[i]);
-		serial->write(',');
-	}
+  for (int i = 0; i < doubles_to_send; i++){
+    serial->print(data[i]);
+    serial->write(',');
+  }
 }
 
 void MatlabTransceiver::sendFooter(){
-	serial->write('Z');
-	serial->println();
+  serial->write('Z');
+  serial->println();
 }
