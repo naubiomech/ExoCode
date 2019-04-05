9c051254fe753667d227f5dbc78dc7d945771857
Jack Garrard <jg2562@nau.edu>
2019-04-04 20:44:56 -0700

    Added more max tests

--- test/tests/utils/testMax.cpp
+++ test/tests/utils/testMax.cpp
@@ -18,3 +18,40 @@ TEST(utils_max_test, zero_test) {
 	EXPECT_EQ(max->getMax(), 0);
 	delete max;
 }
+
+TEST(utils_max_test, low_value_test) {
+	Max* max = new Max();
+	max->update(-100);
+	EXPECT_EQ(max->getMax(), -100);
+	delete max;
+}
+
+TEST(utils_max_test, high_value_test) {
+	Max* max = new Max();
+	max->update(100);
+	EXPECT_EQ(max->getMax(), 100);
+	delete max;
+}
+
+TEST(utils_max_test, replace_value_test) {
+	Max* max = new Max();
+	max->update(-100);
+	max->update(25);
+	EXPECT_EQ(max->getMax(), 25);
+	delete max;
+}
+
+TEST(utils_max_test, nonreplace_value_test) {
+	Max* max = new Max();
+	max->update(100);
+	max->update(-45);
+	EXPECT_EQ(max->getMax(), 100);
+	delete max;
+}
+
+TEST(utils_max_test, float_test) {
+	Max* max = new Max();
+	max->update(0.1);
+	EXPECT_EQ(max->getMax(), 0.1);
+	delete max;
+}
