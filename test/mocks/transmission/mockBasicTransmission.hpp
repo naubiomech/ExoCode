9dd118f3914155c32a3ba2407086bb3794bbe1d7
Jack Garrard <jg2562@nau.edu>
2019-04-10 16:28:34 -0700

    Added a way to check data in mock transmission

--- test/mocks/transmission/mockBasicTransmission.hpp
+++ test/mocks/transmission/mockBasicTransmission.hpp
@@ -6,4 +6,6 @@ public:
 	MockBasicTransmission(Transceiver* trans, unsigned int receive_count, unsigned int send_count):
 		Transmission(trans, NULL, 0, receive_count, send_count){};
 	MOCK_METHOD2(processData, void(ExoMessageBuilder* builder, ExoReport* report));
+	double* getReceiveData(){return this->receive_data;}
+	float* getSendData(){return this->send_data;}
 };
