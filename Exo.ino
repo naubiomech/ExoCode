92a0d71c09e64a7f820c794b60b4a939b0e26bee
Jack Garrard <jg2562@nau.edu>
2019-04-10 13:57:29 -0700

    Changed how board versions operated

--- Exo.ino
+++ Exo.ino
@@ -21,7 +21,7 @@
 //
 // Several parameters can be modified thanks to the Receive and Transmit functions
 #define VERSION 313
-#define TWO_LEG_BOARD
+#define BOARD_VERSION IMU_BOARD
 //The digital pin connected to the motor on/off swich
 const unsigned int zero = 2048;//1540;
 
