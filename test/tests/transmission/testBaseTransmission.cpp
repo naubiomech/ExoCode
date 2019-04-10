f02e747fcd5fb5d359a30467edf2967830fa6a1d
Jack Garrard <jg2562@nau.edu>
2019-04-10 12:19:32 -0700

    Renamed the test files

--- test/tests/transmission/testBaseTransmission.cpp
+++ test/tests/transmission/testBaseTransmission.cpp
@@ -5,16 +5,15 @@
 #include "gmock/gmock.h"
 
 TEST(transmission_base, creation){
-	MockBasicTransmission* trans = new MockBasicTransmission();
+	MockBasicTransmission* trans = new MockBasicTransmission(NULL, 0, 0);
 	delete trans;
 }
 
 TEST(transmission_base, process_zero_args){
 	MockTransceiver* transceiver = new MockTransceiver();
-	MockBasicTransmission* transmission = new MockBasicTransmission(trans, 0, 0);
-	EXPECT_CALL(transmission, processData(NULL,NULL)).Times(1);
-	EXPECT_EQ(1,0);
-	// transmission->process();
+	MockBasicTransmission* transmission = new MockBasicTransmission(transceiver, 0, 0);
+	EXPECT_CALL(*transmission, processData(NULL,NULL)).Times(1);
+	transmission->processData(NULL,NULL);
 	delete transmission;
 	delete transceiver;
 }
