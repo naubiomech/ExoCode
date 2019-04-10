8fac42225c239eadc57a8cdb7f953e32f4871ca6
YF <yf82@nau.edu>
2019-04-10 12:13:54 -0700

    update biofeedback main function

--- Biofeedback_step.ino
+++ Biofeedback_step.ino
@@ -1,46 +1,38 @@
 void takestridetime(Leg* leg) { //take the duration of L/R stride
   leg->Heel_Strike_Count++;
-<<<<<<< HEAD
-//  Serial.println("heel strike count ");
-//  Serial.println(leg->Heel_Strike_Count);
-  
-//  if (leg->Heel_Strike_Count = 1) {
-//    leg->HS1 = millis();
-//  } else if (leg->Heel_Strike_Count = 4) {
-//    leg->HS4 = millis();
-//    leg->stridetime = (leg->HS4 - leg->HS1) / 3; //calculate time (ms) to complete 3 strides
-//  } else 
-=======
-
->>>>>>> ed4cca19684f2e34ce7459442aba3f150b721365
+  if (leg->Heel_Strike_Count == 1) {
+    leg->HS1 = millis();
+  }
+  if (leg->Heel_Strike_Count == 3) {
+    leg->HS3 = millis();
+  }
+  if (leg->Heel_Strike_Count == 4) {
+    leg->HS4 = millis();
+  }
   if (leg->Heel_Strike_Count > 4) {
-    leg->Heel_Strike_Count = 0;
+    leg->Heel_Strike_Count = 1;
+    leg->HS1=0;
+    leg->HS3=0;
+    leg->HS4=0;
   }
   return;
 }
 
 void biofeedback_step_baseline(Leg* leg) {
-  takestridetime(leg);
-  if (leg->Heel_Strike_Count = 1) {
-    leg->HS1 = millis();
-  } else if (leg->Heel_Strike_Count = 4) {
-    leg->HS4 = millis();
-    leg->stridetime_baseline = (leg->HS4 - leg->HS1) / 3 / 1000; //unit:s
-  }
+if (leg->HS1==0 || leg->HS4==0) {
+} else {
+  leg->stridetime_baseline = (leg->HS4 - leg->HS1) / 3 / 1000; //unit:s, average for 3 steps
   leg->stridelength_baseline = treadmill_speed * leg->stridetime_baseline / 1000; //unit:m
   leg->BioFeedback_Baseline_flag = true;
   leg->BIO_BASELINE_FLAG = false;
-//  return;
+}
+  return;
 }
 
 void biofeedback_step_update (Leg* leg) {
-  takestridetime(leg);
-  if (leg->Heel_Strike_Count = 1) {
-    leg->HS1 = millis();
-  } else if (leg->Heel_Strike_Count = 4) {
-    leg->HS4 = millis();
-    leg->stridetime_update = (leg->HS4 - leg->HS1) / 3 / 1000; //unit:s
-  }
+if (leg->HS1==0 || leg->HS4==0) {
+} else {
+  leg->stridetime_update = (leg->HS4 - leg->HS1) / 3 / 1000; //unit:s,average for 2 steps
   leg->stridelength_update = treadmill_speed * leg->stridetime_update / 1000; //unit:m
   leg->stridetime_target = leg->stridetime_baseline * 1.25; //subject to change
 
