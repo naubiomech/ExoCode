5d4e6410fe5c492d7f8924b72b7fd906d77a42b3
Jack Garrard <jg2562@nau.edu>
2019-04-10 12:23:21 -0700

    Fixed the mock basic transmission

--- test/mocks/transmission/mockBasicTransmission.hpp
+++ test/mocks/transmission/mockBasicTransmission.hpp
@@ -1,9 +1,9 @@
 #include <gmock/gmock.h>
 #include "Transmission.hpp"
 
-class MockTransmission: public Transmission {
+class MockBasicTransmission: public Transmission {
 public:
-	MockTransmission(Transceiver* trans, unsigned int receive_count, unsigned int send_count):
-		Transmission(Transceiver* trans, NULL, 0, receive_count, send_count){};
+	MockBasicTransmission(Transceiver* trans, unsigned int receive_count, unsigned int send_count):
+		Transmission(trans, NULL, 0, receive_count, send_count){};
 	MOCK_METHOD2(processData, void(ExoMessageBuilder* builder, ExoReport* report));
-}
+};
