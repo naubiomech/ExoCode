ef5f4ab58fa8d5b58ae9ca37afbccc88d1cb3a77
Jack Garrard <jg2562@nau.edu>
2019-04-10 22:42:54 -0700

    Added more tests for transmission base

--- test/tests/transmission/testBaseTransmission.cpp
+++ test/tests/transmission/testBaseTransmission.cpp
@@ -8,6 +8,7 @@ using::testing::A;
 using::testing::Return;
 using::testing::SetArrayArgument;
 using::testing::ElementsAreArray;
+using::testing::InvokeWithoutArgs;
 
 TEST(transmission_base, allocation){
 	MockBasicTransmission* trans = new MockBasicTransmission(NULL, 0, 0);
@@ -74,14 +75,21 @@ TEST(transmission_base, process_one_send){
 	MockBasicTransmission* transmission = new MockBasicTransmission(transceiver, 0, 1);
 
 	float fake_data[1] = {0};
+	transmission->copyToSend((float*)fake_data);
+	EXPECT_CALL(*transceiver, sendHeader())
+		.Times(1);
+
+	EXPECT_CALL(*transceiver, sendCommand(A<char>()))
+		.Times(1);
+
 	EXPECT_CALL(*transceiver, sendData(A<float*>(),1))
-		.Times(1)
-		.WillRepeatedly(SetArrayArgument<0>(fake_data,fake_data+1));
+		.Times(1);
 
 	EXPECT_CALL(*transceiver, sendFooter())
 		.Times(1);
 
-	EXPECT_CALL(*transmission, processData(NULL,NULL)).Times(1);
+	EXPECT_CALL(*transmission, processData(NULL,NULL))
+		.Times(1);
 
 	transmission->process(NULL,NULL);
 
@@ -95,9 +103,15 @@ TEST(transmission_base, process_ten_send){
 	MockBasicTransmission* transmission = new MockBasicTransmission(transceiver, 0, 10);
 
 	float fake_data[10] = {0,1,2,3,4,5,6,7,8,9};
+	transmission->copyToSend((float*)fake_data);
+	EXPECT_CALL(*transceiver, sendHeader())
+		.Times(1);
+
+	EXPECT_CALL(*transceiver, sendCommand(A<char>()))
+		.Times(1);
+
 	EXPECT_CALL(*transceiver, sendData(A<float*>(),10))
-		.Times(1)
-		.WillRepeatedly(SetArrayArgument<0>(fake_data,fake_data+10));
+		.Times(1);
 
 	EXPECT_CALL(*transceiver, sendFooter())
 		.Times(1);
@@ -110,3 +124,77 @@ TEST(transmission_base, process_ten_send){
 	delete transmission;
 	delete transceiver;
 }
+
+TEST(transmission_base, process_one_both){
+	MockTransceiver* transceiver = new MockTransceiver();
+	MockBasicTransmission* transmission = new MockBasicTransmission(transceiver, 1, 1);
+
+	const float fake_data[] = {0};
+	EXPECT_CALL(*transceiver, receiveData(A<double*>(),1))
+		.Times(1)
+		.WillRepeatedly(SetArrayArgument<0>(fake_data,fake_data+1));
+
+	EXPECT_CALL(*transceiver, receiveFooter())
+		.Times(1)
+		.WillRepeatedly(Return(true));
+
+	EXPECT_CALL(*transceiver, sendHeader())
+		.Times(1);
+
+	EXPECT_CALL(*transceiver, sendCommand(A<char>()))
+		.Times(1);
+
+	EXPECT_CALL(*transceiver, sendData(A<float*>(),1))
+		.Times(1);
+
+	EXPECT_CALL(*transceiver, sendFooter())
+		.Times(1);
+
+	EXPECT_CALL(*transmission, processData(NULL,NULL))
+		.Times(1)
+		.WillOnce(InvokeWithoutArgs(transmission, &MockBasicTransmission::copyReceiveToSend));
+
+	transmission->process(NULL,NULL);
+
+	EXPECT_THAT(fake_data, ElementsAreArray(transmission->getSendData(), 1));
+	delete transmission;
+	delete transceiver;
+}
+
+TEST(transmission_base, process_ten_both){
+	MockTransceiver* transceiver = new MockTransceiver();
+	MockBasicTransmission* transmission = new MockBasicTransmission(transceiver, 10, 10);
+
+	float fake_data[10] = {0,1,2,3,4,5,6,7,8,9};
+	EXPECT_CALL(*transceiver, receiveData(A<double*>(),10))
+		.Times(1)
+		.WillRepeatedly(SetArrayArgument<0>(fake_data,fake_data+10));
+
+	EXPECT_CALL(*transceiver, receiveFooter())
+		.Times(1)
+		.WillRepeatedly(Return(true));
+
+	EXPECT_CALL(*transceiver, sendHeader())
+		.Times(1);
+
+	EXPECT_CALL(*transceiver, sendCommand(A<char>()))
+		.Times(1);
+
+	EXPECT_CALL(*transceiver, sendData(AllOf(A<float*>()),10))
+		.Times(1);
+
+	EXPECT_CALL(*transceiver, sendFooter())
+		.Times(1);
+
+	EXPECT_CALL(*transmission, processData(NULL,NULL))
+		.Times(1)
+		.WillOnce(InvokeWithoutArgs(transmission, &MockBasicTransmission::copyReceiveToSend));
+
+	transmission->process(NULL,NULL);
+
+	EXPECT_THAT(fake_data, ElementsAreArray(transmission->getSendData(), 10));
+	EXPECT_THAT(fake_data, ElementsAreArray(transmission->getReceiveData(), 10));
+	delete transmission;
+	delete transceiver;
+}
+
