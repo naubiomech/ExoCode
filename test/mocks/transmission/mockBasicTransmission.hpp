438a94efb4a6ce35e8d3d67f165e99157dadeb85
Jack Garrard <jg2562@nau.edu>
2019-04-10 21:56:31 -0700

    Added a copy to and from and to each other to mock

--- test/mocks/transmission/mockBasicTransmission.hpp
+++ test/mocks/transmission/mockBasicTransmission.hpp
@@ -6,6 +6,9 @@ public:
 	MockBasicTransmission(Transceiver* trans, unsigned int receive_count, unsigned int send_count):
 		Transmission(trans, NULL, 0, receive_count, send_count){};
 	MOCK_METHOD2(processData, void(ExoMessageBuilder* builder, ExoReport* report));
+	void copyToSend(float* from){Transmission::copyToSend(from);}
+	void copyFromReceive(double* to){Transmission::copyFromReceive(to);}
+	void copyReceiveToSend(){Transmission::copyToSend(this->receive_data);}
 	double* getReceiveData(){return this->receive_data;}
 	float* getSendData(){return this->send_data;}
 };
