16ce82d494f299a648a74c2d805d1ba0d7a9a368
YF <yf82@nau.edu>
2019-04-12 13:04:23 -0700

    modify biofeedback step function to take step length every step instead of every 3 steps

--- Biofeedback_step.ino
+++ Biofeedback_step.ino
@@ -2,47 +2,51 @@ void takestridetime(Leg* leg) { //take the duration of L/R stride
   leg->Heel_Strike_Count++;
   if (leg->Heel_Strike_Count == 1) {
     leg->HS1 = millis();
+    leg->HS4 = 0;
   }
-  if (leg->Heel_Strike_Count == 3) {
-    leg->HS3 = millis();
+  if (leg->Heel_Strike_Count == 2) {
+    leg->HS2 = millis();
   }
   if (leg->Heel_Strike_Count == 4) {
     leg->HS4 = millis();
   }
   if (leg->Heel_Strike_Count > 4) {
-    leg->Heel_Strike_Count = 1;
-    leg->HS1=0;
-    leg->HS3=0;
-    leg->HS4=0;
+    leg->Heel_Strike_Count = 0;
+    leg->HS1 = 0;
+    leg->HS2 = 0;
+    leg->HS4 = 0;
   }
   return;
 }
 
 void biofeedback_step_baseline(Leg* leg) {
-if (leg->HS1==0 || leg->HS4==0) {
-} else {
-  leg->stridetime_baseline = (leg->HS4 - leg->HS1) / 3 / 1000; //unit:s, average for 3 steps
-  leg->stridelength_baseline = treadmill_speed * leg->stridetime_baseline / 1000; //unit:m
-  leg->BioFeedback_Baseline_flag = true;
-  leg->BIO_BASELINE_FLAG = false;
-}
+  if (leg->HS1 == 0 || leg->HS4 == 0) {
+  } else {
+    leg->stridetime_baseline = (leg->HS4 - leg->HS1) / 3 / 1000; //unit:s, average for 3 steps
+    leg->stridelength_baseline = treadmill_speed * leg->stridetime_baseline*100; //unit:cm
+    leg->BioFeedback_Baseline_flag = true;
+    leg->BIO_BASELINE_FLAG = false;
+  }
   return;
 }
 
 void biofeedback_step_update (Leg* leg) {
-if (leg->HS1==0 || leg->HS4==0) {
-} else {
-  leg->stridetime_update = (leg->HS4 - leg->HS1) / 3 / 1000; //unit:s,average for 2 steps
-  leg->stridelength_update = treadmill_speed * leg->stridetime_update / 1000; //unit:m
-  leg->stridetime_target = leg->stridetime_baseline * 1.25; //subject to change
-
-  if (leg->stridetime_update < leg->stridetime_target) {
-    leg->NO_Biofeedback = true;
+  if (leg->HS1 == 0 || leg->HS2 == 0) {
   } else {
-    leg->NO_Biofeedback = false;
-    leg->Frequency = 500;
+    leg->stridetime_update = (leg->HS2 - leg->HS1) / 1000; //unit:s,1 step length
+    leg->stridelength_update = treadmill_speed * leg->stridetime_update*100; //unit:cm
+    leg->stridelength_target = leg->stridelength_baseline * 1.1; //subject to change
+
+    if (leg->stridetime_update < leg->stridetime_target) {
+      leg->NO_Biofeedback = true;
+    } else {
+      leg->NO_Biofeedback = false;
+      leg->Frequency = 100;
+    }
+    leg->Heel_Strike_Count = 0;
+    leg->HS1 = 0;
+    leg->HS2 = 0;
   }
-}
   return;
 }
 
