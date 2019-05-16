d6776197a64c4dc9f57b5ac710b721b455e0e751
Thang <thangnthn@gmail.com>
2019-05-15 18:49:32 -0700

    Changed the value of the flag for the ankle control

--- Variables.h
+++ Variables.h
@@ -98,5 +98,5 @@ bool flag_id = false; // TN 04/29/2019
 
 // Flags for ankle and knee modules // TN 05/06/2019
 
-bool Flag_Ankle_Cfg = true; // TN 05/06/2019
+bool Flag_Ankle_Cfg = false; // TN 05/06/2019
 bool Flag_Knee_Cfg = false; // TN 05/06/2019
