b41e2f1b606bfe82445a009cb1b256a84362b2c9
YF <yf82@nau.edu>
2019-06-10 16:24:32 -0700

    biofeedback variables

--- Variables.h
+++ Variables.h
@@ -79,7 +79,8 @@ double BioFeedback_Freq_min = 200;
 const unsigned int pin_jack = 13;
 unsigned int state = HIGH;
 double right_stride_time, left_stride_time;
-double treadmill_speed = 0.6;//subject to change
+double treadmill_speed = 1;//subject to change
+double BF_scale=1;
 
 // Variables for Human-in-the-Loop Optimization (HLO)
 bool Flag_HLO = false;
