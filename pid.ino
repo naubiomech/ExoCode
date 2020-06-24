23550a61550e5376bd9b98b79096a6a77f635a6c
Greg Orekhov <go226@nau.edu>
2020-06-24 01:06:46 -0700

    Tested and finalized resistance control

--- pid.ino
+++ pid.ino
@@ -22,26 +22,31 @@ void pid(Leg* leg, double input) {
 
   } else {
   }
-  leg->Input = input;
-  leg->pid.Compute_KF(leg->KF);
 
-  if (CURRENT_CONTROL && leg->PID_Setpoint!=0 && MotorParams!=100) {
+  if (CURRENT_CONTROL && leg->PID_Setpoint!=0 && MotorParams!=100) { //Simple Open-Loop Control
     //leg->Vol = ((leg->PID_Setpoint/(TrqConstant * GearRatio * PulleyRatio * MotorEff * GearboxEff))/NomCurrent*2048) + leg->zero; //Setpoint/(Motor torque constant, gear reduction, pulley reduction, motor eff, gearbox eff)
     leg->Vol = (0.37293*(leg->PID_Setpoint))/NomCurrent*2048.0; //Regression control, torque only  
-  } else if (CURRENT_DIAGNOSTICS && MotorParams!=100) {
+    if (Control_Mode == 6 && leg->state == 3) {
+      leg->Vol = -leg->Vol;
+    }
+  } else if (CURRENT_DIAGNOSTICS && MotorParams!=100) { //Diagnostics Mode
     if (leg->Dorsi_Setpoint_Ankle==0) {
       leg->Vol = leg->Setpoint_Ankle/NomCurrent*2048.0; 
     } else {
       leg->Vol = leg->Dorsi_Setpoint_Ankle/NomCurrent*2048.0;
     }
   } else if (MODEL_CONTROL && MotorParams!=100) {
-      leg->Vol = (0.34284*(leg->PID_Setpoint) + 0.023564*(leg->AverageSpeed) + 0.0043038*(leg->PID_Setpoint * leg->AverageSpeed))/NomCurrent*2048; //Regression control, complex model
-  } else {
-    leg->Vol = leg->Output; + leg->zero; //need to map
-  }
-
-  if (Control_Mode == 6 && leg->state == 3) { //Resistance Control
-    leg->Vol = -leg->Vol; // In resistance control during state 3 the desired setpoint is negative of what has been calculated
+    leg->Vol = (0.34284*(leg->PID_Setpoint) + 0.023564*(leg->AverageSpeed) + 0.0043038*(leg->PID_Setpoint * leg->AverageSpeed))/NomCurrent*2048; //Regression control, complex model
+    if (Control_Mode == 6 && leg->state == 3) {
+      leg->Vol = -leg->Vol;
+    }
+  } else { //Closed-Loop Proportional Control
+    if (Control_Mode == 6 && leg->state == 3) {
+      leg->PID_Setpoint = -leg->PID_Setpoint;
+    }
+    leg->Input = input;
+    leg->pid.Compute_KF(leg->KF);
+    leg->Vol = leg->Output; //need to map
   }
 
   leg->Vol = leg->Vol + leg->zero; // Modify the span such that the PWM value is from 0 to 4096.0 instead of -2048.0 to 2048.0
