ee437b05e1ad86ce6a505323d2f32f7372b0529e
Greg Orekhov <go226@nau.edu>
2021-05-18 15:40:31 -0700

    Removed limitation to the motor current span

--- pid.ino
+++ pid.ino
@@ -66,10 +66,6 @@ void pid(Leg* leg, double input) {
 
   leg->Vol = leg->Vol + leg->zero; // Modify the span such that the PWM value is from 0 to 4096.0 instead of -2048.0 to 2048.0
 
-  if (PWM_CONTROL) {
-    if (DEBUG) {Serial.println("In pwm ctrl");}
-    leg->Vol = leg->Vol * 0.8 + 0.1 * 4096.0; // Motor drivers need the PWM to be between 10% and 90%
-  }
   analogWrite(leg->motor_ankle_pin, leg->Vol); //0 to 4096 writing for motor to get Input
   if (DEBUG) {Serial.println("End flag_auto_KF if");}
 } 
