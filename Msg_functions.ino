e56ee12d45eb38dc0bad722ee8db1f79544a22fc
Chance Cuddeback <chancecuddeback@gmail.com>
2021-12-03 14:46:38 -0700

    Modified Mark Functionality

--- Msg_functions.ino
+++ Msg_functions.ino
@@ -10,26 +10,21 @@ void send_data_message_wc() //with COP
 
   //Left Leg
   data_to_send[3] = (left_leg->sign * left_leg->Average_Trq);
-  data_to_send[4] = left_leg->state / 3;
+  if (markFlag) {
+    data_to_send[4] = markCount++;
+    markFlag = false;
+  } else {
+    data_to_send[4] = left_leg->state / 3;
+  }
   data_to_send[5] = (left_leg->sign * left_leg->PID_Setpoint);
 
   //Normalized FSR values, sends mark data in place of left_leg FSR value
   if (right_leg->baseline_value != 0 && left_leg->baseline_value != 0) {
     data_to_send[6] = (right_leg->FSR_Toe_Average) / right_leg->baseline_value;
-    if (markFlag) {
-      data_to_send[7] = markCount;
-      markFlag = false;
-    } else {
-      data_to_send[7] = (left_leg->FSR_Toe_Average) / left_leg->baseline_value;
-    }
+    data_to_send[7] = (left_leg->FSR_Toe_Average) / left_leg->baseline_value;
   } else {
     data_to_send[6] = 0;
-    if (markFlag) {
-      data_to_send[7] = markCount;
-      markFlag = false;
-    } else {
-      data_to_send[7] = 0;
-    }
+    data_to_send[7] = 0;
   }
 
   send_command_message('?', data_to_send, 8);
