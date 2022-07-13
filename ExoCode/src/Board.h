69d79b275359dd026457f5a022efe98dc3ade66c
stegallNAU <paul.stegall@nau.edu>
2022-07-13 14:25:23 -0700

    added rev 0.4 board

--- ExoCode/src/Board.h
+++ ExoCode/src/Board.h
@@ -3,9 +3,10 @@
     
     #define AK_Board_V0_1 1
     #define AK_Board_V0_3 2
+    #define AK_Board_V0_4 3
     
     // TODO : Incorporate into parse INI
-    #define BOARD_VERSION AK_Board_V0_3
+    #define BOARD_VERSION AK_Board_V0_4
 
     #if BOARD_VERSION == AK_Board_V0_1
         #include "Arduino.h"
@@ -148,6 +149,13 @@
                 // Motor enable Pins
                 const unsigned int enable_left_pin[] = {28, 29};
                 const unsigned int enable_right_pin[] = {8, 7};
+                
+                // I2C 
+                // SDA 18
+                // SCL 19
+                
+                //SPI
+                // 
             #endif
             
             const unsigned int motor_enable_on_state = HIGH;
@@ -159,6 +167,111 @@
         {
             const unsigned int ble_signal_pin = 24;
             const unsigned int ble_signal_active = 0;
+            
+            // SPI Conroller Pins
+            const unsigned int miso_pin = 11;
+            const unsigned int mosi_pin= 12;
+            const unsigned int sck_pin = 13;
+            const unsigned int cs_pin = 10;
+            const unsigned int spi_mode = 16;
+            
+            
+        };
+        #endif
+    #elif BOARD_VERSION == AK_Board_V0_4 
+       
+        #include "Arduino.h"
+        namespace logic_micro_pins  //teensy
+        {
+             #if defined(ARDUINO_TEENSY41)
+                // Serial Pins, NC
+                const unsigned int rx1_pin = 0;
+                const unsigned int tx1_pin = 1;
+                
+                // CAN Pins
+                const unsigned int can_rx_pin = 23;
+                const unsigned int can_tx_pin = 22;
+                
+                // FSR Pins
+                const unsigned int fsr_sense_left_heel_pin = A14;
+                const unsigned int fsr_sense_left_toe_pin = A15;
+                const unsigned int fsr_sense_right_heel_pin= A3;
+                const unsigned int fsr_sense_right_toe_pin = A2;
+                
+                // Torque Sensor Pins
+                const unsigned int num_available_joints = 2;
+                const unsigned int torque_sensor_left[] = {A17, A16};
+                //const unsigned int torque_sensor_left1 = A16;
+                const unsigned int torque_sensor_right[] = {A7, A6};
+                //const unsigned int torque_sensor_right1 = A8;
+                
+                
+                // Sync LED Pins
+                const unsigned int sync_led_pin = 15;
+                const unsigned int sync_default_pin = 5;
+            #endif
+            const unsigned int sync_led_on_state = LOW;//HIGH;
+            const unsigned int sync_led_off_state = HIGH;//LOW;
+
+             #if defined(ARDUINO_TEENSY41)
+                // Status LED Pins
+                const unsigned int status_led_r_pin= 14;
+                const unsigned int status_led_g_pin = 25;
+                const unsigned int status_led_b_pin = 24;
+            #endif
+
+            // if you have connected to pins with PWM set to true.
+            const bool status_has_pwm = true;             
+            // For high to be on use 255 for the on state and 0 for the off, for low as on flip it.
+            const uint8_t status_led_on_state = 0;//255;
+            const uint8_t status_led_off_state = 255;//0;  
+                
+            #if defined(ARDUINO_TEENSY41)    
+                // SPI Follower Pins
+                const unsigned int miso_pin = 11;
+                const unsigned int mosi_pin= 12;
+                const unsigned int sck_pin = 13;
+                const unsigned int cs_pin = 10;
+                const unsigned int irq_pin = 36;
+                const unsigned int spi_mode = 16;
+                
+                
+                // Pin to Stop the Motors
+                const unsigned int motor_stop_pin = 9;
+                
+                // Pin to use when we need a value but don't actually want to use it.
+                const unsigned int not_connected_pin = 51;  // selected 51 as it is a pad on the back so I figure it won't hurt anything if something goes wrong.
+                
+                // Motor enable Pins
+                const unsigned int enable_left_pin[] = {28, 29};
+                const unsigned int enable_right_pin[] = {8, 7};
+                
+                // I2C 
+                // SDA 18
+                // SCL 19
+                
+                //SPI
+                // 
+            #endif
+            
+            const unsigned int motor_enable_on_state = HIGH;
+            const unsigned int motor_enable_off_state = LOW;
+        };
+        
+        #if defined(ARDUINO_ARDUINO_NANO33BLE)
+        namespace coms_micro_pins  //nano
+        {
+            const unsigned int ble_signal_pin = 24;
+            const unsigned int ble_signal_active = 0;
+            
+            // SPI Conroller Pins
+            const unsigned int miso_pin = 11;
+            const unsigned int mosi_pin= 12;
+            const unsigned int sck_pin = 13;
+            const unsigned int cs_pin = 10;
+            const unsigned int spi_mode = 16;
+            
+            
         };
         #endif
     #endif
