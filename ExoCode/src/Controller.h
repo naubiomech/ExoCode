20d43bef6620ae40de5758610e3f46db0f6ac939
Chance Cuddeback <chancecuddeback@gmail.com>
2023-02-09 11:41:01 -0700

    Updated Kf bounds for PJMC

--- ExoCode/src/Controller.h
+++ ExoCode/src/Controller.h
@@ -15,7 +15,7 @@
 // Arduino compiles everything in the src folder even if not included so it causes and error for the nano if this is not included.
 #if defined(ARDUINO_TEENSY36)  || defined(ARDUINO_TEENSY41)
 
-#include "Arduino.h"
+#include <Arduino.h>
 
 #include "ExoData.h"
 #include "Board.h"
